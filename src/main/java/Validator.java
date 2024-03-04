d76c480cca650a4c256aabd0390f2f45cbb331f9
ohjuyoungg <ejrm12@naver.com>
2024-03-04 20:41:42 +0900

    fix: 상수 예외 처리에 대한 조건문 버그 수정 if 문의 조건 연산자 버그 수정

--- src/main/java/Validator.java
+++ src/main/java/Validator.java
@@ -3,7 +3,7 @@ public class Validator {
     private final static String BLACK_COLOR = "black";
 
     public static String isColor(String color) {
-        if (color.contains(WHITE_COLOR) || color.contains(BLACK_COLOR)) {
+        if (!(color.equals(WHITE_COLOR)) && !(color.equals(BLACK_COLOR))) {
             throw new IllegalArgumentException("white 와 black 만 작성 가능합니다.");
         }
         return color;
