58421e94d88c01d7f6bba81065692ce9aaabc537
Gyeongtaek <ogt124321@gmail.com>
2024-03-05 00:24:28 +0900

    피드백받은 내용 수정 teamColor를 private으로 수정하였습니다. 체스에서 쓰이는 색이 아닌 다른색이 들어가면 만들어지지않게 체크해주는 rightColor 메서드를 추가하였습니다.

--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -3,14 +3,19 @@ package pieces;
 public class Pawn {
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
-    String teamColor;
+    private String teamColor;
     public Pawn(String teamColor){
-        this.teamColor = teamColor;
+        if (rightColor(teamColor)) {
+            this.teamColor = teamColor;
+        }
     }
     public Pawn(){
         this.teamColor = "white";
     }
     public String getColor(){
         return teamColor;
+    }
+    private boolean rightColor(String teamColor){
+        return teamColor.equals(WHITE_COLOR)||teamColor.equals(BLACK_COLOR);
     }
 }
