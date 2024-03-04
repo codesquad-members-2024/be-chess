117cdc3daf1e62ea64ff0710b5986b7f8977a371
zzawang <hobin0116@naver.com>
2024-03-05 00:08:59 +0900

    feat: 현재 체스 판을 출력하는 기능 추가
    - 현재 체스 판을 8 * 8의 문자열 형태로 생성하여 반환한다.

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,14 +1,17 @@
 package chess;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import pieces.Pawn;
 
 public class Board {
     private ArrayList<Pawn> pawns;
+    private Pawn[][] pieces;
 
     public Board() {
         pawns = new ArrayList<>();
+        pieces = new Pawn[8][8];
     }
 
     public void add(Pawn pawn) {
@@ -24,12 +27,16 @@ public class Board {
     }
 
     public void initialize() {
-        initializePawns(Pawn.WHITE_COLOR, Pawn.WHITE_SYMBOL);
-        initializePawns(Pawn.BLACK_COLOR, Pawn.BLACK_SYMBOL);
+        initializePawns(Pawn.WHITE_COLOR, Pawn.WHITE_SYMBOL, 6);
+        initializePawns(Pawn.BLACK_COLOR, Pawn.BLACK_SYMBOL, 1);
     }
 
-    private void initializePawns(String color, String symbol) {
-        IntStream.range(0, 8).forEach(i -> pawns.add(new Pawn(color, symbol)));
+    private void initializePawns(String color, String symbol, int index) {
+        IntStream.range(0, 8).forEach(i -> {
+            Pawn pawn = new Pawn(color, symbol);
+            pawns.add(pawn);
+            pieces[index][i] = pawn;
+        });
     }
 
     public String getWhitePawnsResult() {
@@ -46,5 +53,13 @@ public class Board {
                 .map(Pawn::getSymbol)
                 .reduce(new StringBuilder(), StringBuilder::append, StringBuilder::append)
                 .toString();
+    }
+
+    public String print() {
+        return IntStream.range(0, 8)
+                .mapToObj(row -> IntStream.range(0, 8)
+                        .mapToObj(col -> pieces[row][col] == null ? " " : pieces[row][col].getSymbol())
+                        .collect(Collectors.joining()))
+                .collect(Collectors.joining("\n"));
     }
 }
