51c3258929bcbf1742a993bf08f4c62757305ab5
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-04 18:20:45 +0900

    refactor : 테스트 내 중복 변수 제거
    - ParameterizedTest를 사용하여 중복 변수 제거

--- app/src/test/java/org/codeSquad/PawnTest.java
+++ app/src/test/java/org/codeSquad/PawnTest.java
@@ -1,14 +1,16 @@
 package org.codeSquad;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PawnTest {
-    @Test
     @DisplayName("흰색 폰이 생성되는지 확인한다")
-    void create(){
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+    @ParameterizedTest
+    @ValueSource(strings = {"white", "black"})
+    void create(String color){
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
 }
