d16c96746f2761b7556a1016baecf03ab9ee4082
Yeriimii <seojs0511@naver.com>
2024-03-04 18:01:55 +0900

    feat: 기물의 색상 상수화
    - 모든 기물의 색상은 white, black 둘 중 하나만 갖는다
    - 기물을 생성할 때 아무것도 입력하지 않으면 기본 색상(white)으로 생성할 수 있다

--- src/main/java/chess/domain/Pawn.java
+++ src/main/java/chess/domain/Pawn.java
@@ -1,13 +1,17 @@
 package chess.domain;
 
 public class Pawn {
-    private String color;
+    private final Color color;
 
-    public Pawn(String color) {
+    public Pawn() {
+        this.color = Color.WHITE;
+    }
+
+    public Pawn(Color color) {
         this.color = color;
     }
 
-    public String getColor() {
+    public Color getColor() {
         return color;
     }
 }
--- src/test/java/chess/domain/PawnTest.java
+++ src/test/java/chess/domain/PawnTest.java
@@ -3,16 +3,26 @@ package chess.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.EnumSource.Mode;
 
 class PawnTest {
-    @DisplayName("흰색 폰이 생성되어야 한다")
+    @DisplayName("지정한 색상의 폰이 생성되어야 한다")
     @ParameterizedTest(name = "기물 색상: {0}")
-    @ValueSource(strings = {"white", "black"})
-    public void create(String color) {
+    @EnumSource(mode = Mode.INCLUDE, names = {"WHITE", "BLACK"})
+    public void create(Color color) {
         Pawn pawn = new Pawn(color);
 
         assertThat(pawn.getColor()).isEqualTo(color);
+    }
+
+    @DisplayName("색상을 주입받지 못해도 기본 색(white)으로 생성될 수 있다")
+    @Test
+    public void create_기본생성자() {
+        Pawn pawn = new Pawn();
+
+        assertThat(pawn.getColor()).isEqualTo(Color.WHITE);
     }
 }
