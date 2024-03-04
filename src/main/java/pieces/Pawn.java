f668de194dd8d1938b53b8790b166e7eb27f8ae9
zzawang <hobin0116@naver.com>
2024-03-04 18:36:12 +0900

    feat: Pawn의 생성자에 체스 심볼 추가
    - 체스 말을 나타낼 심볼을 생성자에 주입할 수 있도록 기존 생성자를 수정하였다.

--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -3,18 +3,27 @@ package pieces;
 public class Pawn {
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
+    public static final String WHITE_SYMBOL = PieceSymbol.WHITE_PAWN.getSymbol();
+    public static final String BLACK_SYMBOL = PieceSymbol.BLACK_PAWN.getSymbol();
 
     private final String color;
+    private final String symbol;
 
     public Pawn() {
         this.color = WHITE_COLOR;
+        this.symbol = WHITE_SYMBOL;
     }
 
-    public Pawn(String color) {
+    public Pawn(String color, String symbol) {
         this.color = color;
+        this.symbol = symbol;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public String getSymbol() {
+        return symbol;
     }
 }
