11f2d8710ed93fbd103e2185b76cbc385b5251e0
ohjuyoungg <ejrm12@naver.com>
2024-03-04 20:21:59 +0900

    feat: white, black 이외의 문자열이 들어올 경우 생성자에서 예외 처리할 수 있도록 Validator 클래스 구현 상수 white, black과 생성자로 넘어온 문자열에 대한 예외 처리

--- src/main/java/Validator.java
+++ src/main/java/Validator.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class ColorValidator {
+public class Validator {
+    private final static String WHITE_COLOR = "white";
+    private final static String BLACK_COLOR = "black";
+
+    public static String isColor(String color) {
+        if (color.contains(WHITE_COLOR) || color.contains(BLACK_COLOR)) {
+            throw new IllegalArgumentException("white 와 black 만 작성 가능합니다.");
+        }
+        return color;
+    }
 }
