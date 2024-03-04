0c39f453fa14250ba9705c3aed89d4610de553f9
zzawang <hobin0116@naver.com>
2024-03-04 18:34:50 +0900

    test: PawnTest에 체스 말의 색깔과 심볼을 검증하는 테스트 추가
    - Pawn 객체를 생성할 때 색깔과 심볼을 주입하고, 예상과 같은지 검증한다.

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -1,29 +1,28 @@
 package pieces;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static pieces.Pawn.BLACK_COLOR;
-import static pieces.Pawn.WHITE_COLOR;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PawnTest {
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
-    public void create() {
-        verifyPawn(WHITE_COLOR);
-        verifyPawn(BLACK_COLOR);
+    public void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
+        assertEquals(Pawn.WHITE_SYMBOL, pawn.getSymbol());
     }
 
     @Test
-    public void create_기본생성자() throws Exception {
-        Pawn pawn = new Pawn();
-        assertEquals(WHITE_COLOR, pawn.getColor());
+    @DisplayName("체스 말의 색깔이 예상과 같은지 검증한다.")
+    public void create() {
+        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_SYMBOL);
+        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_SYMBOL);
     }
 
-    private void verifyPawn(final String color) {
-        Pawn pawn = new Pawn(color);
-        assertThat(pawn.getColor()).isEqualTo(color);
+    void verifyPawn(final String color, final String symbol) {
+        Pawn pawn = new Pawn(color, symbol);
+        assertEquals(color, pawn.getColor());
+        assertEquals(symbol, pawn.getSymbol());
     }
 }
