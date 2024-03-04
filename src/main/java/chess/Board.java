b7baef893865d4af7395d0315714f6ef766c5cde
zzawang <hobin0116@naver.com>
2024-03-04 22:58:15 +0900

    feat: Board에 초기화하는 기능 추가
    - 각 8개의 흰색 Pawn과 검은색 Pawn을 생성해 보유한 말 리스트에 추가한다.
    - 보유하고 있는 흰색 Pawn과 검은색 Pawn을 각각 문자열로 생성한다.

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 import pieces.Pawn;
 
 public class Board {
@@ -20,5 +21,26 @@ public class Board {
 
     public Pawn findPawn(int index) {
         return pawns.get(index);
+    }
+
+    public void initialize() {
+        IntStream.range(0, 8).forEach(i -> pawns.add(new Pawn()));
+        IntStream.range(0, 8).forEach(i -> pawns.add(new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_SYMBOL)));
+    }
+
+    public String getWhitePawnsResult() {
+        return getPawnsResult(Pawn.WHITE_COLOR, Pawn.WHITE_SYMBOL);
+    }
+
+    public String getBlackPawnsResult() {
+        return getPawnsResult(Pawn.BLACK_COLOR, Pawn.BLACK_SYMBOL);
+    }
+
+    private String getPawnsResult(String color, String symbol) {
+        return pawns.stream()
+                .filter(pawn -> pawn.getColor().equals(color) && pawn.getSymbol().equals(symbol))
+                .map(Pawn::getSymbol)
+                .reduce(new StringBuilder(), StringBuilder::append, StringBuilder::append)
+                .toString();
     }
 }
