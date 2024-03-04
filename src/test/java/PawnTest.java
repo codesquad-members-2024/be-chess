3b58dd7a8967412032b089d1a1b69dfcfed4d6e5
ohjuyoungg <ejrm12@naver.com>
2024-03-04 20:22:39 +0900

    feat: Pawn를 통해 생성한 생성자에서 color를 검증하기 위한 PawnTest 클래스 구현 white, black 색상에 따른 생성자 color 객체 테스트

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class PawnTest {
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PawnTest {
+
+    @Test
+    @DisplayName("색상에 따른 폰이 생성되어야 한다")
+    public void create() {
+        verifyPawn("white");
+        verifyPawn("black");
+    }
+
+    private void verifyPawn(final String color) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
+    }
+
+    /*
+    private static Stream<Arguments> isColor_색상검증_실패한다() {
+        return Stream.of(
+                Arguments.of("white"),
+                Arguments.of("black")
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("isColor_색상검증_실패한다")
+    void isColor(String color) {
+        assertThatThrownBy(() -> Validator.isColor(color))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+     */
 }
