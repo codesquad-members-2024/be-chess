df601cfcd137862bf53a349672fd52599a5a8b60
Gyeongtaek <ogt124321@gmail.com>
2024-03-04 13:59:05 +0900

    Pawn class의 생성 폰 객체를 나타내는 클라스 추가 팀 색을 받아서 저장하고 내보낸다.

---
+++ src/Pawn.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Pawn {
+    String teamColor;
+    public Pawn(String teamColor){
+        this.teamColor = teamColor;
+    }
+    public String getColor(){
+        return teamColor;
+    }
+}
