2cf670da2b79b0ef2055b7f760984d5f0f091e2f
Yeriimii <seojs0511@naver.com>
2024-03-04 19:36:18 +0900

    refactor: 테스트 코드 중복 제거
    - @BeforeEach 애너테이션을 활용해 중복되는 체스판 생성 코드를 제거할 수 있다
    - 체스판의 given-when-then 부분을 집약시켜 가독성을 올릴 수 있다

--- src/test/java/chess/board/BoardTest.java
+++ src/test/java/chess/board/BoardTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import chess.common.Color;
 import chess.pieces.Pawn;
 import java.lang.reflect.Method;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,28 +14,31 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class BoardTest {
 
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+    }
+
     @DisplayName("체스판은 기물을 추가할 수 있고, 특정 기물이 몇 번째에 있는지 알 수 있다")
     @Test
     void create() {
         // given
-        Board board = new Board();
         Pawn white = new Pawn(Color.WHITE);
-
-        // when
-        board.add(white);
-
-        // then
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
-
-        // given
         Pawn black = new Pawn(Color.BLACK);
 
         // when
+        board.add(white);
+        int firstAddSize = board.size();
+
         board.add(black);
+        int secondAddSize = board.size();
 
         // then
-        assertEquals(2, board.size());
+        assertEquals(1, firstAddSize);
+        assertEquals(2, secondAddSize);
+        assertEquals(white, board.findPawn(0));
         assertEquals(black, board.findPawn(1));
     }
 
@@ -42,8 +46,6 @@ class BoardTest {
     @ParameterizedTest(name = "체스판에 추가할 값 = {0}")
     @ValueSource(strings = {"1", "2", "3"})
     void add_compileError(String number) {
-        Board board = new Board();
-
         assertThatCode(
                 () -> {
                     Method addMethod = Board.class.getDeclaredMethod("add", Pawn.class);
