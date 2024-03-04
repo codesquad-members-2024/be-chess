faa866d38699cda7903599c45a5eef66666a726e
Gyeongtaek <ogt124321@gmail.com>
2024-03-05 00:01:21 +0900

    Pawn에 팀 색을 나타내는 상수추가 계속해서 중복 사용되는 String인 white와 black을 pawn에 상수로 추가해주었다.

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -1,4 +1,6 @@
 public class Pawn {
+    public static final String WHITE_COLOR = "white";
+    public static final String BLACK_COLOR = "black";
     String teamColor;
     public Pawn(String teamColor){
         this.teamColor = teamColor;
