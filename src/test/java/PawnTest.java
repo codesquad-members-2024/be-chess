0a256a1072fce0993fb7e98ef272b8bc133690a6
zzawang <hobin0116@naver.com>
2024-03-04 16:54:10 +0900

    test: PawnTest에 색이 없는 Pawn을 생성하는 테스트 추가
    - 색이 없는 Pawn을 생성하면 흰색의 말로 생성하는지 검증한다.

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -1,4 +1,7 @@
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static piece.Pawn.BLACK;
+import static piece.Pawn.WHITE;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,10 +11,16 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     public void create() {
-        String whiteColor = "white";
-        String blackColor = "black";
+        String whiteColor = WHITE;
+        String blackColor = BLACK;
         verifyPawn(whiteColor);
         verifyPawn(blackColor);
+    }
+
+    @Test
+    public void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals(WHITE, pawn.getColor());
     }
 
     private void verifyPawn(final String color) {
