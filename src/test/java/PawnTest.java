589b3812bd5c3ec57385e097d4ae7a1c5a00eeb5
sean <tmdgus717@naver.com>
2024-03-04 20:22:33 +0900

    create_기본생성자 메서드 생성
    컴파일 에러 발생 생성자를 따로 구현 하면 기본 생성자는 제공되지 않으므로 기본 생성자를 구현해야한다

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
 
@@ -12,9 +13,15 @@ public class PawnTest {
         verifyPawn(white);
         verifyPawn(black);
     }
-
+    @Test
+    public void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals("white", pawn.getColor());
+    }
     public void verifyPawn(final String color) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
     }
+
+
 }
