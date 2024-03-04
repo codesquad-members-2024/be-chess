59a2594c6ff5406be16964ab2ff824961e8ddbb2
seondays <agseon9@gmail.com>
2024-03-04 18:05:26 +0900

    refactor : 폰 생성 테스트코드 중복 제거
    테스트코드에서 string을 변수로 넣을 수 있도록 수정

--- PawnTest.java
+++ PawnTest.java
@@ -3,10 +3,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PawnTest {
+
+    void verifyPawn(final String color) {
+        Pawn Pawn = new Pawn(color);
+        assertThat(Pawn.getColor()).isEqualTo(color);
+    }
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
+    @DisplayName("생성자로 입력받은 값에 알맞은 색으로 폰이 생성된다.")
     public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+        verifyPawn("white");
+        verifyPawn("black");
     }
 }
