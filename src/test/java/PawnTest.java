68e2c79c50638246a2bd8fd4c312245445ac49d1
yonghyeonpark <pyh4616@naver.com>
2024-03-05 01:18:20 +0900

    test: Pawn 클래스의 유효성 검사 기능 테스트
    폰 객체 생성 시, white 혹은 black이 아닌 색상에 대해 예외가 발생하는지
    테스트합니다.

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class PawnTest {
 
@@ -17,5 +18,12 @@ public class PawnTest {
 
     void verifyPawn(final String color) {
         assertThat(new Pawn(color).getColor()).isEqualTo(color);
+    }
+
+    @Test
+    @DisplayName("흰색 혹은 검은색 폰이 아닌 경우에는 에외가 발생해야 합니다.")
+    void createPawnByNonWhiteOrNonBlackColor() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Pawn("blue"));
     }
 }
