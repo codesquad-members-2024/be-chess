cbcb56cf26868dfb54b1c5d9db8066ccbeade9ba
Yeriimii <seojs0511@naver.com>
2024-03-04 16:58:53 +0900

    refactor: 테스트 코드 리팩토링
    - @ParameterizedTest, @ValueSource를 활용해 중복 테스트를 제거할 수 있다

--- src/test/java/chess/domain/PawnTest.java
+++ src/test/java/chess/domain/PawnTest.java
@@ -3,13 +3,16 @@ package chess.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PawnTest {
-    @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
-    public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+    @ParameterizedTest(name = "기물 색상: {0}")
+    @ValueSource(strings = {"white", "black"})
+    public void create(String color) {
+        Pawn pawn = new Pawn(color);
+
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
 }
