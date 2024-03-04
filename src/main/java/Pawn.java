b5911da1be137eae3c7a97b4db6169580e0ceedf
Miensoap <miensop51@gmail.com>
2024-03-04 16:18:58 +0900

    Gradle 사용하도록 변경
    intellij 설정에서 maven 사용해 lib 추가 -> gradle dependencies 추가로 변경

---
+++ src/main/java/Pawn.java
@@ -0,0 +1,17 @@
+public class Pawn {
+    enum Color {WHITE , BLACK}
+
+    private final Color color;
+
+    public Pawn(Color color) {
+        this.color = color;
+    }
+
+    public Pawn(){
+        this.color = Color.WHITE;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
