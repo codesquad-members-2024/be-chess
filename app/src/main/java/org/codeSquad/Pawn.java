895efba62d0b5706d29170ead871d406b8eb1483
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-04 17:59:00 +0900

    feat : 특정 색상을 생성자에서 주입받는 pawn 객체 추가
    - 문자열 'color'를 생성자로 주입받는다
    - 위 'color'를 자신의 속성으로 지니고 있다

---
+++ app/src/main/java/org/codeSquad/Pawn.java
@@ -0,0 +1,13 @@
+package org.codeSquad;
+
+public class Pawn {
+    private String color;
+
+    public Pawn(String color){
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
