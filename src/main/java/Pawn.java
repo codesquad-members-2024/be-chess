4fce62af40af1dc933805f2e7b707a0338171a4e
yonghyeonpark <pyh4616@naver.com>
2024-03-05 00:39:31 +0900

    refactor: Pawn 클래스, color에 대한 유효성 검사 기능 추가
    객체 생성 시, 파라미터로 들어오는 color에 대해 유효성 검사를 진행합니다.
    => white 혹은 black이 아니라면 예외가 발생합니다.

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -1,9 +1,18 @@
 public class Pawn {
 
+    private static final String COLOR_ERROR_MESSAGE = "[ERROR] 색상은 white 혹은 black만 가능합니다.";
+
     private final String color;
 
     public Pawn(String color) {
+        validate(color);
         this.color = color;
+    }
+
+    private void validate(String color) {
+        if (!color.equals("white") && !color.equals("black")) {
+            throw new IllegalArgumentException(COLOR_ERROR_MESSAGE);
+        }
     }
 
     public String getColor() {
