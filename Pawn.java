63450513a387397e7bbd1743b3285c1dfe39618a
sean <tmdgus717@naver.com>
2024-03-04 15:03:42 +0900

    PawnTest클래스 생성 후 create() 메서드 작성
    Pawn 객체 생성과 getColor 를 통해 올바르게 Pawn 객체가 생성 되었는지 확인하는 테스트 코드 작성

---
+++ Pawn.java
@@ -0,0 +1,12 @@
+public class Pawn {
+    public String getColor() {
+        return color;
+    }
+
+    private String color; // white or black
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+}
---
+++ PawnTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class PawnTest {
+    @Test
+    @DisplayName("흰색 폰이 생성되어야 한다")
+    public void create() {
+        String color = "white";
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo("white");
+//        assertThat(pawn.getColor()).isEqualTo("black");
+    }
+
+}
