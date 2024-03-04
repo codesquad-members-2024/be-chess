d064189633026540f56824793e1c035ba9050ef4
zzawang <hobin0116@naver.com>
2024-03-04 12:15:18 +0900

    test: Pawn 객체의 색깔을 검증하는 테스트 기능 추가
    - create() 메소드를 통해 Pawn 객체를 생성하고, getter 메소드를 통해 색깔을 가져와 예상한 결과와 같은지 검증한다.
    - 흰색 말을 생성하고 색깔이 "white"와 같은지 검증한다.

---
+++ test/PawnTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import piece.Pawn;
+
+public class PawnTest {
+    @Test
+    @DisplayName("흰색 폰이 생성되어야 한다")
+    public void create() {
+        Pawn pawn = new Pawn("white");
+        assertThat(pawn.getColor()).isEqualTo("white");
+    }
+}
