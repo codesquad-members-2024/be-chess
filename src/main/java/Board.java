8b4ffa3ede3d7e79720e89c473d9d4764997f9d5
yonghyeonpark <pyh4616@naver.com>
2024-03-05 02:14:58 +0900

    feat: Board 클래스 구현
    Pawn 타입의 리스트인 pawns를 필드로 가지며, 객체 생성 시, 초기화됩니다.
    addPawn(): pawns에 파라미터로 넘어온 pawn을 추가합니다.
    getPawnsSize(): pawns의 크기를 반환합니다.
    findPawn(): 파라미터로 넘어온 인덱스에 해당하는 Pawn 객체를 반환합니다.

---
+++ src/main/java/Board.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+
+    private final List<Pawn> pawns;
+
+    public Board() {
+        this.pawns = new ArrayList<>();
+    }
+
+    public void addPawn(Pawn pawn) {
+        pawns.add(pawn);
+    }
+
+    public int getPawnsSize() {
+        return pawns.size();
+    }
+
+    public Pawn findPawn(int index) {
+        return pawns.get(index);
+    }
+}
