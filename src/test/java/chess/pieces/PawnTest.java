f2552da4462c6df040143b39abe47d47a2c6ecb3
kimjooyoung <kjy915875@gmail.com>
2024-03-05 20:39:51 +0900

    Board클래스에 initialize, print메서드 추가
    Pawn을 초기화해주는 initialize, 화면에 체스판을 그려주는 print메서드 작성

--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -11,13 +11,14 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     void create() {
-        varifiyPawn(Color.WHITE);
-        varifiyPawn(Color.BLACK);
+        verifyPawn(Color.WHITE, '♟');
+        verifyPawn(Color.BLACK, '♙');
     }
 
-    void varifiyPawn(Color color) {
+    void verifyPawn(Color color, char represent) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
+        assertThat(pawn.getRepresent()).isEqualTo(represent);
     }
 
     @Test
