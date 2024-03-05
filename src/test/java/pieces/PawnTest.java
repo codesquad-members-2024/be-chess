9a6fdbbcffb6453f18d6aa2e58d7194b8de35bc8
Gyeongtaek <ogt124321@gmail.com>
2024-03-05 11:54:27 +0900

    PawnTest에 심볼체크 추가 PawnTest에 Pawn에서 추가한 representation이 추가되고 알맞게 가져오는지 체크한다.

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -10,14 +10,13 @@ class PawnTest {
     @Test
     @DisplayName("흰색 폰과 검은색 폰이 생성되어야한다.")
     public void create(){
-        final String BLACK = "black";
-        final String WHITE = "white";
-        verifyPawn(BLACK);
-        verifyPawn(WHITE);
+        verifyPawn(Pawn.BLACK_COLOR,Pawn.BLACK_REPRESENTATION);
+        verifyPawn(Pawn.WHITE_COLOR,Pawn.WHITE_REPRESENTATION);
     }
-    private void verifyPawn(final String color){
-        Pawn pawn = new Pawn(color);
-        assertThat(pawn.getColor()).isEqualTo(color);
+    private void verifyPawn(final String color, final char representation) {
+        Pawn pawn = new Pawn(color, representation);
+        assertEquals(color, pawn.getColor());
+        assertEquals(representation, pawn.getRepresentation());
     }
 
     @Test
@@ -25,5 +24,6 @@ class PawnTest {
     public void defaultPawn() throws Exception {
         Pawn pawn = new Pawn();
         assertEquals("white", pawn.getColor());
+        assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation());
     }
 }
