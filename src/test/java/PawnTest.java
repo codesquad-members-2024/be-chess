5cf2f97843d7d6e907ce08f7206ecd7d78296fd0
ehpark <silbroeh@gmail.com>
2024-03-05 11:10:01 +0900

    feat : Pawn 기본 생성자 구현
    매개변수가 없는 생성자의 경우 흰색의 Pawn이 생성되도록 기본 생성자 구현

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -2,9 +2,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PawnTest {
-
     @Test
     @DisplayName("지정된 색상의 폰이 생성되어야 한다.")
     void create() {
@@ -13,6 +13,13 @@ class PawnTest {
 
         verifyPawn(white);
         verifyPawn(black);
+    }
+
+    @Test
+    @DisplayName("기본 생성자로 생성할 경우 흰색의 폰이 생성되어야 한다.")
+    void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals("white", pawn.getColor());
     }
 
     void verifyPawn(final String color) {
