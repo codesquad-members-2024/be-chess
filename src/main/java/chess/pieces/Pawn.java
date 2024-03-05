dfbe4bc0bae57e0cc54f6ad64f079da97895b703
kimjooyoung <kjy915875@gmail.com>
2024-03-05 20:49:22 +0900

    pawn의 repersent심볼을 final static으로 저장

--- src/main/java/chess/pieces/Pawn.java
+++ src/main/java/chess/pieces/Pawn.java
@@ -4,12 +4,15 @@ import chess.enums.Color;
 
 public class Pawn {
 
+    public static final char WHITE_REPRESENT = '♟';
+    public static final char BLACK_REPRESENT = '♙';
+
     private final Color color;
     private final char represent;
 
     public Pawn(Color color) {
         this.color = color;
-        this.represent = color.equals(Color.WHITE) ? '♟' : '♙';
+        this.represent = color.equals(Color.WHITE) ? WHITE_REPRESENT : BLACK_REPRESENT;
     }
 
     public Pawn() {
--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -11,8 +11,8 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     void create() {
-        verifyPawn(Color.WHITE, '♟');
-        verifyPawn(Color.BLACK, '♙');
+        verifyPawn(Color.WHITE, Pawn.WHITE_REPRESENT);
+        verifyPawn(Color.BLACK, Pawn.BLACK_REPRESENT);
     }
 
     void verifyPawn(Color color, char represent) {
