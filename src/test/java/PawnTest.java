64a8a385053f23ebb31be6ba3c7c6fae1b088588
KDT live <dnjsxo0616@gmail.com>
2024-03-05 15:05:29 +0900

    feat: 색이 없는 Pawn 테스트 추가

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
 
@@ -12,6 +13,12 @@ public class PawnTest {
     void verifyPawn(final String color) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
+    }
+
+    @Test
+    public void createDefaultConstructor() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals("white", pawn.getColor());
     }
 
 }
