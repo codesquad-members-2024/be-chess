25a72c33fcdf9b3bd8f705c2c412f14dc2371795
KDT live <dnjsxo0616@gmail.com>
2024-03-05 15:39:57 +0900

    refactor: WHITE -> WHITE_COLOR, BLACK -> BLACK_COLOR 상수 이름 변경

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -1,10 +1,10 @@
 public class Pawn {
-    public static final String WHITE = "white";
-    public static final String BLACK = "black";
+    public static final String WHITE_COLOR = "white";
+    public static final String BLACK_COLOR = "black";
     private final String color;
 
     public Pawn() {
-        this.color = WHITE;
+        this.color = WHITE_COLOR;
     }
 
     public Pawn(final String color) {
