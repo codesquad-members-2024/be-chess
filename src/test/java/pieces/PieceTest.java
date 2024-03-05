cc0b125d2b4e39fd1d29fffc9c25e66f6dc8c047
zzawang <hobin0116@naver.com>
2024-03-05 13:03:33 +0900

    test: PieceTest에 각 Piece 객체 생성 테스트 추가
    - 각 Piece별로 흰색 말과 검은 색 말을 생성하고 알맞게 생성됐는지 검증한다.

--- src/test/java/pieces/PieceTest.java
+++ src/test/java/pieces/PieceTest.java
@@ -7,22 +7,28 @@ import org.junit.jupiter.api.Test;
 
 public class PieceTest {
     @Test
-    @DisplayName("기본 생성자로 Pawn객체를 생성하면 하얀색 말로 초기화되어야 한다.")
-    public void create_기본생성자() throws Exception {
-        Piece piece = new Piece();
-        assertEquals(Piece.WHITE_COLOR, piece.getColor());
-        assertEquals(Piece.WHITE_SYMBOL, piece.getSymbol());
+    @DisplayName("pawn, knight, rook, bishop, queen, king 기물들의 색깔과 유니코드 심볼이 알맞게 생성되어야 한다.")
+    public void create_piece() {
+        verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_SYMBOL);
+        verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_SYMBOL);
+
+        verifyPiece(Piece.createWhiteRook(), Piece.WHITE_COLOR, Piece.WHITE_ROOK_SYMBOL);
+        verifyPiece(Piece.createBlackRook(), Piece.BLACK_COLOR, Piece.BLACK_ROOK_SYMBOL);
+
+        verifyPiece(Piece.createWhiteKnight(), Piece.WHITE_COLOR, Piece.WHITE_KNIGHT_SYMBOL);
+        verifyPiece(Piece.createBlackKnight(), Piece.BLACK_COLOR, Piece.BLACK_KNIGHT_SYMBOL);
+
+        verifyPiece(Piece.createWhiteBishop(), Piece.WHITE_COLOR, Piece.WHITE_BISHOP_SYMBOL);
+        verifyPiece(Piece.createBlackBishop(), Piece.BLACK_COLOR, Piece.BLACK_BISHOP_SYMBOL);
+
+        verifyPiece(Piece.createWhiteQueen(), Piece.WHITE_COLOR, Piece.WHITE_QUEEN_SYMBOL);
+        verifyPiece(Piece.createBlackQueen(), Piece.BLACK_COLOR, Piece.BLACK_QUEEN_SYMBOL);
+
+        verifyPiece(Piece.createWhiteKing(), Piece.WHITE_COLOR, Piece.WHITE_KING_SYMBOL);
+        verifyPiece(Piece.createBlackKing(), Piece.BLACK_COLOR, Piece.BLACK_KING_SYMBOL);
     }
 
-    @Test
-    @DisplayName("체스 말의 색깔이 예상과 같은지 검증한다.")
-    public void create() {
-        verifyPawn(Piece.WHITE_COLOR, Piece.WHITE_SYMBOL);
-        verifyPawn(Piece.BLACK_COLOR, Piece.BLACK_SYMBOL);
-    }
-
-    void verifyPawn(final String color, final String symbol) {
-        Piece piece = new Piece(color, symbol);
+    private void verifyPiece(final Piece piece, final String color, final String symbol) {
         assertEquals(color, piece.getColor());
         assertEquals(symbol, piece.getSymbol());
     }
