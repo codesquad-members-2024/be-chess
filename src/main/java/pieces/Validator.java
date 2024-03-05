b580b411a8961c0589d0c75167f735ea66f794b9
ohjuyoungg <ejrm12@naver.com>
2024-03-05 16:41:41 +0900

    feat: 폰에 기본 생성자 추가 기본 생성자 추가 및 1차 코드 리뷰 반영한 enum 상수 구현

--- src/main/java/pieces/Validator.java
+++ src/main/java/pieces/Validator.java
@@ -1,9 +1,6 @@
 public class Validator {
-    private final static String WHITE_COLOR = "white";
-    private final static String BLACK_COLOR = "black";
-
     public static String isColor(String color) {
-        if (!(color.equals(WHITE_COLOR)) && !(color.equals(BLACK_COLOR))) {
+        if (!(color.equals(Pawn.Color.BLACK)) && !(color.equals(Pawn.Color.WHITE))) {
             throw new IllegalArgumentException("white 와 black 만 작성 가능합니다.");
         }
         return color;
