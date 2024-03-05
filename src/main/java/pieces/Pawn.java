ef528c6b2d61cc31a14c40997826fb9ab17a717d
Gyeongtaek <ogt124321@gmail.com>
2024-03-05 11:48:54 +0900

    Pawn의 피스를 나타내는 심볼 추가 흰색과 검은색 Pawn 심볼을 상수로 추가해주고 체스 피스 심볼을 받아오고 내보내는 코드를 추가해주었습니다.

--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -3,19 +3,30 @@ package pieces;
 public class Pawn {
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
+    public static final char WHITE_REPRESENTATION = 'P';
+    public static final char BLACK_REPRESENTATION = 'p';
     private String teamColor;
-    public Pawn(String teamColor){
-        if (rightColor(teamColor)) {
+    private char pieceRepresent;
+    public Pawn(String teamColor, char pieceRepresent){
+        if (rightColor(teamColor) && rightPiece(pieceRepresent)) {
             this.teamColor = teamColor;
+            this.pieceRepresent = pieceRepresent;
         }
     }
     public Pawn(){
-        this.teamColor = "white";
+        this.teamColor = WHITE_COLOR;
+        this.pieceRepresent = WHITE_REPRESENTATION;
     }
     public String getColor(){
         return teamColor;
     }
-    private boolean rightColor(String teamColor){
+    public char getRepresentation(){
+        return pieceRepresent;
+    }
+    private boolean rightColor(String teamColor){ //팀 색이 올바른지 체크한다.
         return teamColor.equals(WHITE_COLOR)||teamColor.equals(BLACK_COLOR);
+    }
+    private boolean rightPiece(char pieceRepresent){ //피스들의 심블이 올바른지 체크한다.
+        return pieceRepresent == WHITE_REPRESENTATION||pieceRepresent == BLACK_REPRESENTATION;
     }
 }
