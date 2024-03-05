4908ee5213f293ce96fb7e6e42f007f99a228817
yonghyeonpark <pyh4616@naver.com>
2024-03-05 22:28:13 +0900

    refactor: 테스트코드 리팩토링
    하나의 테스트 함수는 한 가지 경우의 수만 테스트하도록 수정했습니다.
    표시 이름에는 부정문이 들어가지 않도록 하고, 명확하게 표현했습니다.
    중복되는 부분은 BeforeEach를 이용해 공통 처리했습니다.

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -2,24 +2,40 @@ package chess;
 
 import chess.pieces.Pawn;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
 
-    @Test
-    @DisplayName("폰 객체를 체스판에 추가하고, 저장된 폰의 수와 저장된 객체를 확인합니다.")
-    public void verifyResultOfAddingPawnToBoard() {
-        Board board = new Board();
+    private Board board;
+    private Pawn white;
+    private Pawn black;
 
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+        white = new Pawn(Pawn.WHITE_COLOR);
+        black = new Pawn(Pawn.BLACK_COLOR);
+    }
+
+    @Test
+    @DisplayName("체스판에 저장된 폰의 수를 확인합니다.")
+    public void verifyNumberOfPawnsOnBoard() {
         board.addPawn(white);
         Assertions.assertThat(board.getPawnsSize()).isEqualTo(1);
-        Assertions.assertThat(board.findPawn(0)).isEqualTo(white);
 
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
         board.addPawn(black);
         Assertions.assertThat(board.getPawnsSize()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("체스판에 저장된 객체를 확인합니다.")
+    public void verifyStoredPawnObjectsOnBoard() {
+        board.addPawn(white);
+        Assertions.assertThat(board.findPawn(0)).isEqualTo(white);
+
+        board.addPawn(black);
         Assertions.assertThat(board.findPawn(1)).isEqualTo(black);
     }
 }
--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -9,30 +9,29 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 public class PawnTest {
 
     @Test
-    @DisplayName("흰색 혹은 검은색 폰이 정상적으로 생성되어야 합니다.")
-    void createPawnByWhiteAndBlackColor() {
+    @DisplayName("흰색 폰이 정상적으로 생성되어야 합니다.")
+    void createPawnByWhiteColor() {
         String color = Pawn.WHITE_COLOR;
-        verifyPawn(color);
-
-        color = Pawn.BLACK_COLOR;
-        verifyPawn(color);
-    }
-
-    void verifyPawn(final String color) {
-        assertThat(new Pawn(color).getColor()).isEqualTo(color);
+        assertThat(new Pawn(color).verifyPawn(color)).isTrue();
     }
 
     @Test
-    @DisplayName("흰색 혹은 검은색 폰이 아닌 경우에는 에외가 발생해야 합니다.")
-    void createPawnByNonWhiteOrNonBlackColor() {
+    @DisplayName("검은색 폰이 정상적으로 생성되어야 합니다.")
+    void createPawnByBlackColor() {
+        String color = Pawn.BLACK_COLOR;
+        assertThat(new Pawn(color).verifyPawn(color)).isTrue();
+    }
+
+    @Test
+    @DisplayName("색상이 흰색 혹은 검은색인 경우에만 폰 객체가 생성됩니다.")
+    void createPawnByNonWhiteAndNonBlackColor() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Pawn("blue"));
     }
 
     @Test
-    @DisplayName("기본 생성자로 chess.pieces.Pawn 객체를 생성하면 색상은 흰색이어야 합니다.")
+    @DisplayName("기본 생성자로 Pawn 객체를 생성하면 색상은 흰색이어야 합니다.")
     public void createPawnBy기본생성자() {
-        Pawn pawn = new Pawn();
-        assertThat(pawn.getColor()).isEqualTo(Pawn.WHITE_COLOR);
+        assertThat(new Pawn().verifyPawn(Pawn.WHITE_COLOR)).isTrue();
     }
 }
