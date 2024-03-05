5fdbd753bd566de319b5ff8f1f519a58495982e4
ohjuyoungg <ejrm12@naver.com>
2024-03-05 16:45:34 +0900

    feat: 체스 게임의 체스판 구현 BoardTest 클래스의 Board에 대한 메소드와 요구사항을 반영한 클래스 구현

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,2 +1,25 @@
-package chess;public class Board {
+package chess;
+
+import pieces.Pawn;
+
+import java.util.ArrayList;
+
+public class Board {
+    private final ArrayList<Pawn> pawns;
+
+    public Board() {
+        pawns = new ArrayList<>();
+    }
+
+    void add(Pawn pawn) {
+        pawns.add(pawn);
+    }
+
+    int size() {
+        return pawns.size();
+    }
+
+    Pawn findPawn(int index) {
+        return pawns.get(index);
+    }
 }
