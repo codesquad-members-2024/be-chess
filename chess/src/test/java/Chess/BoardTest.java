b2ec375f87eb3281ac21ad27bf37162a748500ad
kalia <fmoon11200@naver.com>
2024-03-05 14:44:52 +0900

    추가 테스트 및 중복 제거

--- chess/src/test/java/Chess/BoardTest.java
+++ chess/src/test/java/Chess/BoardTest.java
@@ -7,10 +7,15 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardTest {
+    private Board board;
+
+    @BeforeEach
+    public void setUp() {
+        board = new Board();
+    }
+
     @Test
     public void create() throws Exception {
-        Board board = new Board();
-
         Pawn white = new Pawn(Pawn.WHITE_COLOR);
         board.add(white);
         assertEquals(1, board.size());
@@ -20,5 +25,8 @@ public class BoardTest {
         board.add(black);
         assertEquals(2, board.size());
         assertEquals(black, board.findPawn(1));
+
+        // Integer integer = new Integer("7");
+        // board.add(integer); // 컴파일 에러 발생
     }
 }
