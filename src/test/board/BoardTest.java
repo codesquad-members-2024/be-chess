d96d829a9ff156efb86fece6b9bc1be2b95e4152
seondays <agseon9@gmail.com>
2024-03-05 15:59:48 +0900

    refactor : 체스판 테스트코드 중복 최소화
    코드의 중복을 제거해서 새로운 메서드를 생성, 체스판은 전체 한번만 생성될 수 있도록 조정

--- src/test/board/BoardTest.java
+++ src/test/board/BoardTest.java
@@ -2,6 +2,7 @@ package src.test.board;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
 import src.chess.board.Board;
 import src.chess.pieces.Pawn;
 import org.junit.jupiter.api.DisplayName;
@@ -9,19 +10,26 @@ import org.junit.jupiter.api.Test;
 
 public class BoardTest {
 
+    private static Board board;
+
     @Test
-    @DisplayName("체스판을 만들고 정상적으로 말을 추가할 수 있는지 확인한다.")
-    public void create() throws Exception {
-        Board board = new Board();
+    @DisplayName("체스판을 생성한다.")
+    @BeforeAll
+    public static void create() {
+        board = new Board();
+    }
 
-        Pawn white = new Pawn();
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
+    @Test
+    @DisplayName("정상적으로 말을 추가할 수 있는지 확인한다.")
+    public void add() throws Exception {
+        addPawn("white",1,0);
+        addPawn("black",2,1);
+    }
 
-        Pawn black = new Pawn("black");
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
+    public void addPawn(String color, int size, int index) {
+        Pawn pawn = new Pawn(color);
+        board.add(pawn);
+        assertEquals(size, board.size());
+        assertEquals(pawn, board.findPawn(index));
     }
 }
