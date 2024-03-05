0de386ace9e8375c24238284a886341c0e253ba4
Daniel <923daniel@naver.com>
2024-03-06 08:53:38 +0900

    refactor: @Before 애노테이션을 활용해 중복을 제거 - 계속 사용될 board 객체를 Before에노테이션을 이용해 미리 생성한다.

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -5,10 +5,16 @@ import pieces.Pawn;
 import static org.junit.jupiter.api.Assertions.assertEquals; // JUnit 5의 assertEquals를 임포트
 
 public class BoardTest {
+
+    Board board;
+
+    @BeforeEach
+    public void initialize() {
+        this.board = new Board();
+    }
+
     @Test
     public void create() throws Exception {
-        Board board = new Board();
-
         Pawn white = new Pawn(Pawn.COLOR_WHITE);
         board.add(white);
         assertEquals(1, board.size());
