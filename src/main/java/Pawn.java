8d91e5bf04aed30dff8d4272d3cf00ce8fee6d99
KDT live <dnjsxo0616@gmail.com>
2024-03-05 15:09:54 +0900

    feat: white 폰을 생성하는 기본 생성자 추가 및 상수 선언

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -1,5 +1,11 @@
 public class Pawn {
+    public static final String WHITE = "white";
+    public static final String BLACK = "black";
     private final String color;
+
+    public Pawn() {
+        this.color = WHITE;
+    }
 
     public Pawn(final String color) {
         this.color = color;
