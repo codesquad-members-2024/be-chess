aa65dd440edcb79c33f7cf1551c33e29ed6f96f1
choejieun <wldms5221@icloud.com>
2024-03-05 18:22:36 +0900

    Pawn색을 설정하는 클래스 enum으로 색을 만들어 매개변수로 받고 리턴

---
+++ src/main/java/Pawn.java
@@ -0,0 +1,11 @@
+public class Pawn {
+    PieceColor color;
+
+    public Pawn(PieceColor color) {
+      this.color = color;
+    }
+
+    public PieceColor getColor() {
+        return color;
+    }
+}
