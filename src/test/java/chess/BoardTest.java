1a901cf205e524daa1a5d78fdc473f7ad36bf6a8
Daniel <923daniel@naver.com>
2024-03-06 08:50:25 +0900

    feat: BoardTest 클래스 구현 - 직접 Board를 생성해서 Pawn을 넣은 뒤 보드의 크기와, Pawn이 잘   들어갔는지 확인.

---
+++ src/test/java/chess/BoardTest.java
@@ -0,0 +1,22 @@
+package chess;
+import org.junit.jupiter.api.*;
+import pieces.Pawn;
+
+import static org.junit.jupiter.api.Assertions.assertEquals; // JUnit 5의 assertEquals를 임포트
+
+public class BoardTest {
+    @Test
+    public void create() throws Exception {
+        Board board = new Board();
+
+        Pawn white = new Pawn(Pawn.COLOR_WHITE);
+        board.add(white);
+        assertEquals(1, board.size());
+        assertEquals(white, board.findPawn(0));
+
+        Pawn black = new Pawn(Pawn.COLOR_BLACK);
+        board.add(black);
+        assertEquals(2, board.size());
+        assertEquals(black, board.findPawn(1));
+    }
+}
