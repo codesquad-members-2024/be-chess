6ca97e984a2ca4fdda8e281dc3228aa52e0bbdea
Miensoap <miensop51@gmail.com>
2024-03-05 19:49:18 +0900

    Step5 구현
    1. 기물의 색, 기물의 종류에 따른 enum 구현
    2. Piece에 대한 색과 기물에 따라 분리된 팩토리 메소드에서 enum 사용
    3. 팩토리 메소드 리팩토링 : createWhite , Black 사용해 중복된 파라미터 제거
    4. 체스판의 빈칸은 Black piece로 채우도록 변경
    5. 지정한  색과 종류에 일치하는 기물들의 수를 세는 메서드 추가
    6. 주어진 위치의 기물을 조회하는 메서드 추가
    7. 임의의 위치에 기물을 추가하는 메서드 추가
    8. 보드의 색상별 기물들의 점수 계산 메서드 추가
    9. 보드의 기물들을 정렬하는 메서드 추가

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static utils.StringUtils.appendNewLine;
 
 public class BoardTest {
@@ -16,7 +18,7 @@ public class BoardTest {
 
     @BeforeEach
     @AfterEach
-    void clearBoard(){
+    void clearBoard() {
         board = new Board();
     }
 
@@ -25,12 +27,13 @@ public class BoardTest {
     public void initialize() throws Exception {
         Board board = new Board();
         board.init();
-        assertEquals("pppppppp", board.getWhitePawnsResult());
-        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
+        assertEquals("♙♙♙♙♙♙♙♙", board.getWhitePawnsResult());
+        assertEquals("♟♟♟♟♟♟♟♟", board.getBlackPawnsResult());
     }
+
     @Test
     @DisplayName("보드에 기물이 정상적으로 추가되어야 한다")
-    void addPawn(){
+    void addPawn() {
         verifyAddPawn(Piece::createWhitePawn);
         verifyAddPawn(Piece::createBlackPawn);
     }
@@ -39,20 +42,86 @@ public class BoardTest {
         int size = board.size();
         Piece piece = createPiece.get();
         board.add(piece);
-        assertEquals(size+1, board.size());
+        assertEquals(size + 1, board.size());
     }
 
     @Test
     void create() throws Exception {
         board.init();
         assertEquals(32, board.size());
-        String blankRank = appendNewLine("........");
+        String blankRank = appendNewLine("        ");
         assertEquals(
-            appendNewLine("RNBQKBNR") +
-            appendNewLine("PPPPPPPP") +
-            blankRank + blankRank + blankRank + blankRank +
-            appendNewLine("pppppppp") +
-            appendNewLine("rnbqkbnr"),
-            board.showBoard());
+                appendNewLine("♜♞♝♛♚♝♞♜") +
+                        appendNewLine("♟♟♟♟♟♟♟♟") +
+                        blankRank + blankRank + blankRank + blankRank +
+                        appendNewLine("♙♙♙♙♙♙♙♙") +
+                        appendNewLine("♖♘♗♕♔♗♘♖"),
+                board.showBoard());
+    }
+
+    @Test
+    void move() throws Exception {
+        board.initEmpty();
+
+        String position = "b5";
+        Piece piece = Piece.createBlackRook();
+        board.addPieceAt(position, piece);
+
+        assertEquals(piece, board.findPiece(position));
+        System.out.println(board.showBoard());
+    }
+
+    @Test
+    void findPiece() throws Exception {
+        board.init();
+
+        assertEquals(Piece.createBlackRook(), board.findPiece("a8"));
+        assertEquals(Piece.createBlackRook(), board.findPiece("h8"));
+        assertEquals(Piece.createWhiteRook(), board.findPiece("a1"));
+        assertEquals(Piece.createWhiteRook(), board.findPiece("h1"));
+    }
+
+    @Test
+    void calculatePoint() throws Exception {
+        board.initEmpty();
+
+        addPiece("b6", Piece.createBlackPawn());
+        addPiece("e6", Piece.createBlackQueen());
+        addPiece("b8", Piece.createBlackKing());
+        addPiece("c8", Piece.createBlackRook());
+        addPiece("b4", Piece.createBlackPawn());
+        addPiece("b3", Piece.createBlackPawn());
+
+        addPiece("f2", Piece.createWhitePawn());
+        addPiece("g2", Piece.createWhitePawn());
+        addPiece("e1", Piece.createWhiteRook());
+        addPiece("f1", Piece.createWhiteKing());
+
+        assertEquals(15.5, board.calculatePoint(Piece.Color.BLACK), 0.01);
+        assertEquals(7.0, board.calculatePoint(Piece.Color.WHITE), 0.01);
+
+        System.out.println(board.showBoard());
+    }
+
+    private void addPiece(String position, Piece piece) {
+        board.addPieceAt(position, piece);
+    }
+
+    @Test
+    @DisplayName("보드의 기물들을 정렬했을 때 점수가 높은 순서 , 검은색 -> 흰색 순서로 정렬 되어야 함")
+    void sortPieces(){
+        addPiece("b6", Piece.createBlackPawn());
+        addPiece("e6", Piece.createBlackQueen());
+        addPiece("f2", Piece.createWhitePawn());
+        addPiece("e1", Piece.createWhiteRook());
+
+        List<Piece> result = List.of(
+                board.findPiece("e6"),
+                board.findPiece("b6"),
+                board.findPiece("e1"),
+                board.findPiece("f2")
+        );
+
+        assertThat(board.sortPieces()).isEqualTo(result);
     }
 }
--- src/test/java/chess/pieces/PieceTest.java
+++ src/test/java/chess/pieces/PieceTest.java
@@ -1,8 +1,7 @@
 package chess.pieces;
 
 import chess.pieces.Piece.Color;
-import chess.pieces.Piece.Name;
-import chess.pieces.Piece.represent;
+import chess.pieces.Piece.Type;
 import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.*;
@@ -13,11 +12,12 @@ public class PieceTest {
     @Test
     @DisplayName("생성자에 입력한 색상의 기물이 생성되어야 하고 , 색상에 따른 표시 문자가 설정되어야 한다.")
     void create() {
-        verifyPawn(Piece.createWhitePawn(), Color.WHITE , represent.WHITE_PAWN_REPRESENTATION.getValue());
-        verifyPawn(Piece.createBlackPawn(), Color.BLACK, represent.BLACK_PAWN_REPRESENTATION.getValue());
+        verifyPiece(Piece.createWhitePawn(), Color.WHITE , Type.PAWN.getRepresentation());
+        verifyPiece(Piece.createBlackPawn(), Color.BLACK, String.valueOf((char)(Type.PAWN.getRepresentation().charAt(0)+6)));
+        verifyPiece(Piece.createBlank(), Color.NOCOLOR, Type.BLANK.getRepresentation());
     }
 
-    private void verifyPawn(Piece piece, final Color color, final String representation) {
+    private void verifyPiece(Piece piece, final Color color, final String representation) {
         assertEquals(color, piece.getColor());
         assertEquals(representation, piece.getRepresentation());
     }
