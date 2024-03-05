d2733b34c47cb78ad7690cbb39c643ce15d2221e
seondays <agseon9@gmail.com>
2024-03-05 15:09:50 +0900

    feat : 체스판 클래스 구현
    체스판 역할을 하는 클래스, 폰을 추가하고 인덱스로 찾을 수 있는 기능을 가진다.

---
+++ Board.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Board {
+    private final ArrayList<Pawn> pawns = new ArrayList<>();
+
+    public void add(Pawn pawn) {
+        pawns.add(pawn);
+    }
+
+    public int size() {
+        return pawns.size();
+    }
+
+    public Pawn findPawn(int index) {
+        return pawns.get(index);
+    }
+}
