a1e0c5af0d6f4ab65fa9c0356bd48c3cf7894dc9
WonTae Han <57781744+dnjsxo0616@users.noreply.github.com>
2024-03-05 12:22:32 +0900

    [애셔] step-1 체스 프로젝트 시작 (#26)
    * feat: gradle 추가
    
    * feat: Pawn 클래스 생성 테스트 추가
    
    * feat: Pawn 생성 클래스 추가
    
    * refactor: gitignore 수정 및 PawnTest 하드코딩 제거
    
    * refactor: .gradle 디렉토리 제거

---
+++ src/main/java/Pawn.java
@@ -0,0 +1,11 @@
+public class Pawn {
+    private final String color;
+
+    public Pawn(final String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
