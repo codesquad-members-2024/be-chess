c11ebca8b3026a3d17978ea624985fc86dca85c9
ehpark <silbroeh@gmail.com>
2024-03-05 15:01:43 +0900

    test : 추가 테스트 작성
    Pawn 이외의 다른 객체를 Board에 추가하면 컴파일 에러가 발생하는지 확인

--- src/chess/BoardTest.java
+++ src/chess/BoardTest.java
@@ -1,5 +1,6 @@
 package chess;
 
+import org.junit.jupiter.api.DisplayName;
 import pieces.Pawn;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class BoardTest {
 
     @Test
+    @DisplayName("Pawn 추가 시 갯수 및 추가한 객체 확인")
     void create() throws Exception {
         Board board = new Board();
 
@@ -20,5 +22,14 @@ class BoardTest {
         board.add(black);
         assertEquals(2, board.size());
         assertEquals(black, board.findPawn(1));
+    }
+
+    @Test
+    @DisplayName("Pawn 이외의 다른 객체를 추가하면 컴파일 에러가 발생한다.")
+    void addNotPawnObject() {
+        Board board = new Board();
+        Integer integer = Integer.valueOf("7");
+
+        board.add(integer); // 컴파일 에러 발생
     }
 }
