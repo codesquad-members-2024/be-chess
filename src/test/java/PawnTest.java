fb79b1b82aec47069f37ea079dbcefde53c7757f
sean <tmdgus717@naver.com>
2024-03-05 14:24:06 +0900

    PawnTest 흰색과 검은색 상수로 선언
    final을 사용하여
    private static final String WHITE
    private static final String BLACK
    선언

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -3,26 +3,26 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
+    private static final String WHITE = "white";
+    private static final String BLACK = "black";
 
     @Test
     @DisplayName("입력받은 색의 폰이 생성되어야 한다")
     public void create() {
-        String white = "white";
-        String black = "black";
-
-        verifyPawn(white);
-        verifyPawn(black);
+        verifyPawn(WHITE);
+        verifyPawn(BLACK);
     }
+
     @Test
     @DisplayName("기본 생성자 구현 테스트")
     public void create_기본생성자() throws Exception {
         Pawn pawn = new Pawn();
-        assertEquals("white", pawn.getColor());
+        assertEquals(WHITE, pawn.getColor());
     }
+
     public void verifyPawn(final String color) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
     }
-
 
 }
