b592ab7c056141ecc49573c7ad5102ee015320c1
kimjooyoung <kjy915875@gmail.com>
2024-03-05 20:47:20 +0900

    start, end 입력 기능 구현
    사용자의 입력을 받고 게임을 진행하는 Game 클래스 구현

--- src/main/java/chess/Game.java
+++ src/main/java/chess/Game.java
@@ -1,7 +1,37 @@
 package chess;
 
+import chess.enums.Order;
+
+import java.util.Scanner;
+
 public class Game {
 
+    public void run() {
+        try {
+            Order start = userInput();
+            if (start.equals(Order.START)) {
+                Board board = new Board();
+                board.initialize();
+                board.print();
+            } else {
+                return;
+            }
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
 
+    }
 
+    private Order userInput() throws IllegalArgumentException{
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        Order result;
+        try {
+            result = Order.valueOf(input.toUpperCase());
+        } catch (IllegalArgumentException ex) {
+            throw new IllegalArgumentException("잘못된 입력입니다");
+        }
+        return result;
+    }
 }
--- src/main/java/chess/Main.java
+++ src/main/java/chess/Main.java
@@ -3,6 +3,7 @@ package chess;
 public class Main {
 
     public static void main(String[] args) {
-
+        Game chess = new Game();
+        chess.run();
     }
 }
