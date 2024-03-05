d602192478afd389e8b272839d65a2bf395ccd24
Yeriimii <seojs0511@naver.com>
2024-03-05 23:54:07 +0900

    feat: `Piece`의 팩토리 메서드 업데이트
    - 모든 기물을 생성할 수 있도록 추가
    - `PieceShape` 클래스도 팩토리 메서드 추가에 대응해서 enum 요소 추가

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -7,6 +7,10 @@ import chess.common.Color;
 public class Piece {
     public static final String ALLOWED_PAWN_NAME = "pawn";
     public static final String ALLOWED_KNIGHT_NAME = "knight";
+    public static final String ALLOWED_BISHOP_NAME = "bishop";
+    public static final String ALLOWED_ROOK_NAME = "rook";
+    public static final String ALLOWED_QUEEN_NAME = "queen";
+    public static final String ALLOWED_KING_NAME = "king";
     private final Color color;
     private final String name;
 
@@ -29,6 +33,38 @@ public class Piece {
 
     public static Piece createBlackKnight() {
         return new Piece(BLACK, ALLOWED_KNIGHT_NAME);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(WHITE, ALLOWED_BISHOP_NAME);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(BLACK, ALLOWED_BISHOP_NAME);
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(WHITE, ALLOWED_ROOK_NAME);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(BLACK, ALLOWED_ROOK_NAME);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(WHITE, ALLOWED_QUEEN_NAME);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(BLACK, ALLOWED_QUEEN_NAME);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(WHITE, ALLOWED_KING_NAME);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(BLACK, ALLOWED_KING_NAME);
     }
 
     public Color getColor() {
