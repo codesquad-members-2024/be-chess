4ec2bccf0e66ca933bc6bd6fd40cd5f14bd497d3
zzawang <hobin0116@naver.com>
2024-03-05 14:20:34 +0900

    feat: Board에 전체 기물의 상태를 알 수 있도록 기능 추가
    - initialize() 메소드에서는 8*8의 보드판에 전체 기물이 알맞은 위치에 있도록 배치한다.
    - pieceCount() 메소드에서는 현재 보유하고 있는 기물의 수를 구해 반환한다.
    - showBoard() 메소드에서는 8*8의 보드판의 현재 상태를 그대로 문자열로 생성하여 반환한다.

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -18,48 +18,69 @@ public class Board {
         pieces.add(piece);
     }
 
-    public int size() {
-        return pieces.size();
-    }
-
-    public Piece findPawn(int index) {
-        return pieces.get(index);
-    }
-
     public void initialize() {
-        initializePawns(Piece.WHITE_COLOR, Piece.WHITE_SYMBOL, 6);
-        initializePawns(Piece.BLACK_COLOR, Piece.BLACK_SYMBOL, 1);
+        initializeExceptPawns(Piece.WHITE_COLOR, 7);
+        initializePawns(Piece.WHITE_COLOR, 6);
+        initializePawns(Piece.BLACK_COLOR, 1);
+        initializeExceptPawns(Piece.BLACK_COLOR, 0);
     }
 
-    private void initializePawns(String color, String symbol, int index) {
-        IntStream.range(0, 8).forEach(i -> {
-            Piece piece = new Piece(color, symbol);
-            pieces.add(piece);
-            board[index][i] = piece;
+    private void initializePawns(String color, int row) {
+        IntStream.range(0, 8).forEach(col -> {
+            Piece piece = generatePawn(color);
+            add(piece);
+            board[row][col] = piece;
         });
     }
 
-    public String getWhitePawnsResult() {
-        return getPawnsResult(Piece.WHITE_COLOR, Piece.WHITE_SYMBOL);
+    private Piece generatePawn(String color) {
+        if (color.equals(Piece.WHITE_COLOR)) {
+            return Piece.createWhitePawn();
+        }
+        return Piece.createBlackPawn();
     }
 
-    public String getBlackPawnsResult() {
-        return getPawnsResult(Piece.BLACK_COLOR, Piece.BLACK_SYMBOL);
+    private void initializeExceptPawns(String color, int row) {
+        Piece[] piecesExceptPawns = generatePiecesExceptPawns(color);
+        IntStream.range(0, 8).forEach(col -> {
+            add(piecesExceptPawns[col]);
+            board[row][col] = piecesExceptPawns[col];
+        });
     }
 
-    private String getPawnsResult(String color, String symbol) {
-        return pieces.stream()
-                .filter(piece -> piece.getColor().equals(color) && piece.getSymbol().equals(symbol))
-                .map(Piece::getSymbol)
-                .reduce(new StringBuilder(), StringBuilder::append, StringBuilder::append)
-                .toString();
+    private Piece[] generatePiecesExceptPawns(String color) {
+        Piece[] result = new Piece[8];
+        if (color.equals(Piece.WHITE_COLOR)) {
+            result[0] = Piece.createWhiteRook();
+            result[1] = Piece.createWhiteKnight();
+            result[2] = Piece.createWhiteBishop();
+            result[3] = Piece.createWhiteQueen();
+            result[4] = Piece.createWhiteKing();
+            result[5] = Piece.createWhiteBishop();
+            result[6] = Piece.createWhiteKnight();
+            result[7] = Piece.createWhiteRook();
+        } else if (color.equals(Piece.BLACK_COLOR)) {
+            result[0] = Piece.createBlackRook();
+            result[1] = Piece.createBlackKnight();
+            result[2] = Piece.createBlackBishop();
+            result[3] = Piece.createBlackQueen();
+            result[4] = Piece.createBlackKing();
+            result[5] = Piece.createBlackBishop();
+            result[6] = Piece.createBlackKnight();
+            result[7] = Piece.createBlackRook();
+        }
+        return result;
     }
 
-    public String print() {
+    public int pieceCount() {
+        return pieces.size();
+    }
+
+    public String showBoard() {
         return IntStream.range(0, 8)
                 .mapToObj(row -> IntStream.range(0, 8)
                         .mapToObj(col -> board[row][col] == null ? " " : board[row][col].getSymbol())
                         .collect(Collectors.joining()))
-                .collect(Collectors.joining("\n"));
+                .collect(Collectors.joining("\n")).concat("\n");
     }
 }
