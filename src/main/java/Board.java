7290ddffde20f301a62b24e334f241f997801a51
KDT live <dnjsxo0616@gmail.com>
2024-03-05 16:28:08 +0900

    feat: Board 클래스 추가 - Pawn 객체를 저장하고 추가 - findPawn : 주어진 인덱스의 Pawn을 반환 - size : 현재 Pawn의 수 반환

---
+++ src/main/java/Board.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+
+    private final List<Pawn> board = new ArrayList<>();
+
+    public void add(final Pawn pawn) {
+        board.add(pawn);
+    }
+
+    public Pawn findPawn(final int findIndex) {
+        return board.get(findIndex);
+    }
+
+    public int size() {
+        return board.size();
+    }
+}
