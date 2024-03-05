9acf9199aa1a24b567cc232f096ed7ccd90730ca
Daniel <923daniel@naver.com>
2024-03-06 01:20:08 +0900

    refactor: PawnTest 클래스 create 매소드 수정 - whiteColor 매개변수 생성 - blackColor 매개변수 생성

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -6,9 +6,11 @@ public class PawnTest {
     @Test
     @DisplayName("흰색/검은색 폰이 생성되어야 한다")
     public void create() {
+        String whiteColor = "white";
+        String blackColor = "black";
 
-        verifyPawn("white");
-        verifyPawn("black");
+        verifyPawn(whiteColor);
+        verifyPawn(blackColor);
     }
 
     private void verifyPawn(String color) {
