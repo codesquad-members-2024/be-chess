06a19394bfd739e9a7405e45947fd84b1f9ca843
sean <tmdgus717@naver.com>
2024-03-05 14:50:47 +0900

    BoardTest create 메서드 생성 후 실패 확인
    Board 클래스 생성 후 add, size, findPawn 함수 구현 해야함

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,18 +1,18 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BoardTest {
-//    @Test
-//    public void create() {
-//        Board board = new Board();
-//
-//        Pawn white = new Pawn(Pawn.WHITE);
-//        board.add(white);
-//        assertEquals(1,board.size());
-//        assertEquals(white,board.findPawn(0));
-//
-//        Pawn black = new Pawn(Pawn.BLACK);
-//        board.add(black);
-//        assertEquals(2,board.size());
-//        assertEquals(white,board.findPawn(1));
-//    }
+    @Test
+    public void create() {
+        Board board = new Board();
+
+        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        board.add(white);
+        assertEquals(1,board.size());
+        assertEquals(white,board.findPawn(0));
+
+        Pawn black = new Pawn(Pawn.BLACK_COLOR);
+        board.add(black);
+        assertEquals(2,board.size());
+        assertEquals(white,board.findPawn(1));
+    }
 }
