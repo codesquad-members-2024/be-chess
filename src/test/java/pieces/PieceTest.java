7d9a94ed98cba83bc9e2e4f69912ef16dca16726
zzawang <hobin0116@naver.com>
2024-03-05 14:31:27 +0900

    test: PieceTest에 검은색 말과 흰색 말을 구분하는 테스트 추가
    - Piece의 메소드 isWhite()와 isBlack()을 검증한다.

--- src/test/java/pieces/PieceTest.java
+++ src/test/java/pieces/PieceTest.java
@@ -1,6 +1,8 @@
 package pieces;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,5 +33,17 @@ public class PieceTest {
     private void verifyPiece(final Piece piece, final String color, final String symbol) {
         assertEquals(color, piece.getColor());
         assertEquals(symbol, piece.getSymbol());
+    }
+
+    @Test
+    @DisplayName("검은색 말과 흰색 말을 구분할 수 있다.")
+    public void is_검은색_또는_하얀색() throws Exception {
+        Piece whitePiece = Piece.createWhiteBishop();
+        assertTrue(whitePiece.isWhite());
+        assertFalse(whitePiece.isBlack());
+
+        Piece blackPiece = Piece.createBlackBishop();
+        assertTrue(blackPiece.isBlack());
+        assertFalse(blackPiece.isWhite());
     }
 }
