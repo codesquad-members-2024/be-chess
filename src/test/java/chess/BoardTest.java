ea2c3d141c9eae859de9b21d94f9e3840e757b03
ohjuyoungg <ejrm12@naver.com>
2024-03-05 16:47:46 +0900

    feat: 체스 보드판에 대한 보드 테스트 구현 중복되는 부분 제거하여 리팩토링 @Before 애노테이션 활용해 중복 제거 보드 리스트의 처음 초깃값이 0인지 확인하는 테스트 코드 작성

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,2 +1,39 @@
-package chess;public class BoardTest {
+package chess;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pieces.Pawn;
+import pieces.Pawn.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoardTest {
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+    }
+
+    @Test
+    @DisplayName("보드 리스트의 처음 초기값이 0인지 확인한다")
+    void initialSize() {
+        board = new Board();
+        assertEquals(0, board.size());
+    }
+
+    @Test
+    @DisplayName("보드 생성 후 폰을 색상별로 추가한다")
+    void create() {
+        Pawn white = new Pawn(Color.WHITE);
+        board.add(white);
+        assertEquals(1, board.size());
+        assertEquals(white, board.findPawn(0));
+
+        Pawn black = new Pawn(Pawn.Color.BLACK);
+        board.add(black);
+        assertEquals(2, board.size());
+        assertEquals(black, board.findPawn(1));
+    }
 }
