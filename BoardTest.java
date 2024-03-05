21528a130bd9132f3a82e263f6083ee664952aba
seondays <agseon9@gmail.com>
2024-03-05 14:49:06 +0900

    test : 체스 판 생성을 테스트하기 위한 테스트 코드 작성
    해당 코드는 체스 판이 정상적으로 생성되는지, 체스 판에 폰을 놓을 수 있는지를 확인한다.

---
+++ BoardTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoardTest {
+
+    public void create() throws Exception {
+        Board board = new Board();
+
+        Pawn white = new Pawn();
+        board.add(white);
+        assertEquals(1, board.size());
+        assertEquals(white, board.findPawn(0));
+
+        Pawn black = new Pawn("black");
+        board.add(black);
+        assertEquals(2, board.size());
+        assertEquals(black, board.findPawn(1));
+    }
+}
