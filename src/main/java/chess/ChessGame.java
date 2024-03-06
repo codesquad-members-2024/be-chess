e3b67b3295b6ca5d3446aee59154999c19a24851
KDT live <dnjsxo0616@gmail.com>
2024-03-06 21:34:37 +0900

    feat: main 메소드를 가지는 ChessGame 클래스 추가 - Scanner를 활용해 사용자 입력 값을 받는다. - while을 통해서 게임 종료는 end, 게임 시작은 start

---
+++ src/main/java/chess/ChessGame.java
@@ -0,0 +1,28 @@
+package chess;
+
+import java.util.Scanner;
+
+public class ChessGame {
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ChessGame chessGame = new ChessGame();
+        chessGame.play();
+    }
+
+    public void play() {
+        Board board = new Board();
+        board.initialize();
+        while(true) {
+            System.out.println("게임시작 start, 게임 종료 end 입력하세요.");
+            String userInput = sc.nextLine();
+            if(userInput.equals("end")) {
+                return;
+            } else if (userInput.equals("start")) {
+                String print = board.print();
+                System.out.println(print);
+            }
+        }
+    }
+
+}
