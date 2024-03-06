3cfa9b3cb6e574c91b97417130ccc8acb7c9710f
KDT live <dnjsxo0616@gmail.com>
2024-03-06 17:36:29 +0900

    refactor: Pawn 클래스 상수를 enum 상수로 변경

--- src/main/java/chess/pieces/Pawn.java
+++ src/main/java/chess/pieces/Pawn.java
@@ -1,40 +1,19 @@
 package chess.pieces;
 
 public class Pawn {
-    public static final String WHITE_COLOR = "white";
-    public static final String BLACK_COLOR = "black";
     public static final Character WHITE_REPRESENTATION = 'p';
     public static final Character BLACK_REPRESENTATION = 'P';
-    private final String color;
-    private final Character representation;
+    private final PawnColor color;
     public Pawn() {
-        this.color = WHITE_COLOR;
-        this.representation = WHITE_REPRESENTATION;
+        this.color = PawnColor.WHITE;
     }
 
-    public Pawn(final String color) {
+    public Pawn(final PawnColor color) {
         this.color = color;
-        this.representation = checkColor(color);
-
     }
 
-    public Pawn(String color, char representation) {
-        this.color = color;
-        this.representation = representation;
-    }
-
-    public String getColor() {
+    public PawnColor getColor() {
         return color;
     }
 
-    public Character getRepresentation() {
-        return representation;
-    }
-
-    private Character checkColor(String color) {
-        if(color.equals(Pawn.WHITE_COLOR)) {
-            return WHITE_REPRESENTATION;
-        }
-        return BLACK_REPRESENTATION;
-    }
 }
