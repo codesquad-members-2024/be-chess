eef7f8f4f58ba39fce1914aeb194a001171c1031
Cat Hanbit <ryudb030@gmail.com>
2024-03-06 17:35:00 +0900

    폰 객체, 색 검증 중복제거함
    중복된 코드는 보이지만 어떻게 제거해야할지 막막하고
    모든 필요한 객체를 매개변수로 넘겨줄 생각을 했는데
    메서드내에서 접근가능한 클래스를 만들수있다
    신기

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -8,13 +8,24 @@ public class PawnTest {
     @DisplayName("흰색 폰이 생성되어야함")
     void create() {
         String color = "White";
-        Pawn pawn = new Pawn(color);
-        assertThat(pawn.getColor()).isEqualTo(color);
-
+        verifyPawn(color);
 
         color = "Black";
-        Pawn pawn2 = new Pawn(color);
-        assertThat(pawn2.getColor()).isEqualTo(color);
+        verifyPawn(color);
+        
+//        메서드를 만들면 항상 매개변수로 넘겨줄 생각을함
+        Pawn pawn = new Pawn(color);
+        verifyPawn(pawn, color);
+    }
+
+//    컨닝 ㅋㅋ
+    void verifyPawn(final String color) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
+    }
+
+    void verifyPawn(Pawn pawn, final String color) {
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
 
 }
