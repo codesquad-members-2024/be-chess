abce91684c388f2b6fad5ff4e0a545d4a0f05601
ohjuyoungg <ejrm12@naver.com>
2024-03-06 23:37:41 +0900

    test: Pawn에 추가된 기물에 대한 테스트 검증
    assertEqauls() 메소드를 모두 assertThat()으로 변경

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -1,37 +1,31 @@
 package pieces;
 
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import pieces.Pawn.Color;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class PawnTest {
 
     @Test
     void create_기본생성자() {
         Pawn pawn = new Pawn();
-        assertEquals(Color.WHITE, pawn.getColor());
+        assertThat(pawn.getColor()).isEqualTo(Color.WHITE);
     }
 
     @Test
-    @DisplayName("색상에 따른 폰이 생성되어야 한다")
+    @DisplayName("색상과 출력 부분에 맞게 폰이 생성되었는지 검증한다")
     void create() {
-        verifyPawn(Color.WHITE);
-        verifyPawn(Color.BLACK);
+        assertAll(() ->
+                        verifyPawn(Color.WHITE, "p"),
+                () -> verifyPawn(Color.BLACK, "P"));
     }
 
-    private void verifyPawn(Color color) {
+    private void verifyPawn(Color color, final String representation) {
         Pawn pawn = new Pawn(color);
-        assertThat(pawn.getColor()).isSameAs(color);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"red", "yellow", "blue", "", " "})
-    void isColor(String color) {
-        assertThatThrownBy(() -> Validator.isColor(Color.valueOf(color)))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertAll(() -> assertThat(pawn.getColor()).isEqualTo(color),
+                () ->
+                        assertThat(pawn.getColor().getRepresentation()).isEqualTo(representation));
     }
 }
