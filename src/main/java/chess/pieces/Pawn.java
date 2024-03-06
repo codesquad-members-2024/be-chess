4e1ba8fa3383bb2edfd62f9d721c4d363ffa40a3
KDT live <dnjsxo0616@gmail.com>
2024-03-06 11:20:49 +0900

    feat: Pawn 색에 따른 문자 부여 - 검은색 Pawn 출력은 대문자 'P' - 흰색 Pawn의 출력은 소문자 'p'

--- src/main/java/chess/pieces/Pawn.java
+++ src/main/java/chess/pieces/Pawn.java
@@ -3,17 +3,38 @@ package chess.pieces;
 public class Pawn {
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
+    public static final Character WHITE_REPRESENTATION = 'p';
+    public static final Character BLACK_REPRESENTATION = 'P';
     private final String color;
-
+    private final Character representation;
     public Pawn() {
         this.color = WHITE_COLOR;
+        this.representation = WHITE_REPRESENTATION;
     }
 
     public Pawn(final String color) {
         this.color = color;
+        this.representation = checkColor(color);
+
+    }
+
+    public Pawn(String color, char representation) {
+        this.color = color;
+        this.representation = representation;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public Character getRepresentation() {
+        return representation;
+    }
+
+    private Character checkColor(String color) {
+        if(color.equals(Pawn.WHITE_COLOR)) {
+            return WHITE_REPRESENTATION;
+        }
+        return BLACK_REPRESENTATION;
     }
 }
