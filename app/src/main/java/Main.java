d83bbc8151b19a4037200c6af08ef4cdbd5b60bb
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-06 23:12:00 +0900

    feat : 게임 시작 및 종료 기능 구현
    - start 명령 입력시 게임 시작, end 명령 입력시 게임 종료
    - Scanner를 이용한 사용자 입력

---
+++ app/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import chess.GameExecutor;
+
+public class Main {
+    public static void main(String[] args) {
+        GameExecutor gameExecutor = new GameExecutor();
+        gameExecutor.run();
+    }
+}
