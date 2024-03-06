4dd826da3bd12c382e08c827d6c1ef35c2fd2a52
seondays <agseon9@gmail.com>
2024-03-06 12:20:39 +0900

    test : 보드판 초기화 관련 테스트 코드 작성
    해당 코드들은 8개의 폰을 색에 맞춰서 생성할 수 있는지, 폰을 추가한 상태로 전체 체스판을 추가할 수 있는지 확인한다.

--- src/test/board/BoardTest.java
+++ src/test/board/BoardTest.java
@@ -2,7 +2,8 @@ package src.test.board;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import src.chess.Colors;
 import src.chess.board.Board;
 import src.chess.pieces.Pawn;
 import org.junit.jupiter.api.DisplayName;
@@ -14,22 +15,38 @@ public class BoardTest {
 
     @Test
     @DisplayName("체스판을 생성한다.")
-    @BeforeAll
-    public static void create() {
+    @BeforeEach
+    public void create() {
         board = new Board();
     }
 
     @Test
     @DisplayName("정상적으로 말을 추가할 수 있는지 확인한다.")
     public void add() throws Exception {
-        addPawn("white",1,0);
-        addPawn("black",2,1);
+        addPawn(Colors.WHITE, 1, 0);
+        addPawn(Colors.BLACK, 2, 1);
     }
 
-    public void addPawn(String color, int size, int index) {
+    public void addPawn(Colors color, int size, int index) {
         Pawn pawn = new Pawn(color);
         board.add(pawn);
         assertEquals(size, board.size());
         assertEquals(pawn, board.findPawn(index));
+    }
+
+    @Test
+    @DisplayName("체스판에 폰을 추가한 상태로 초기화가 되는지 확인한다.")
+    public void print() {
+        board.initialize();
+        board.print();
+    }
+
+    @Test
+    @DisplayName("정상적으로 8개의 폰들을 생성해낼 수 있는지 확인한다.")
+    public void initialize() throws Exception {
+        Board board = new Board();
+        board.initialize();
+        assertEquals("pppppppp", board.getWhitePawnsResult());
+        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
     }
 }
