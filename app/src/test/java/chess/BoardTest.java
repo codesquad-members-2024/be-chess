e2d22940383c63b47c148ecd5bde74a8ed0266a7
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-06 21:45:18 +0900

    test : 체스 보드 초기화 기능 테스트 코드 추가
    - initialize 메서드 호출 후 print 메서드 호출 시 상황 테스트
    - 기존 @Nested 테스트 환경에 추가 테스트 코드 기재

--- app/src/test/java/chess/BoardTest.java
+++ app/src/test/java/chess/BoardTest.java
@@ -1,20 +1,17 @@
 package chess;
 
-import chess.Board;
-import chess.pieces.ChessPieceColor;
+import chess.pieces.Color;
 import chess.pieces.Pawn;
 import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Nested
+@DisplayName("Board 클래스는 생성 후")
 public class BoardTest {
     private Board board;
-
-    @Nested
-    @DisplayName("Board 클래스는 생성 후")
-    class CreatingBoard {
-        @BeforeEach
-        void setBoard(){
+    @BeforeEach
+    void setBoard(){
              board = new Board();
         }
 
@@ -35,26 +32,24 @@ public class BoardTest {
             @Test
             @DisplayName("폰의 색상은 하얀색이다")
             void findPawnColorOnBoard(){
-                assertThat(board.findPawn(0)).isEqualTo(ChessPieceColor.WHITE.getColorName());
+                assertThat(board.findPawn(0)).isEqualTo(Color.WHITE.getColorName());
             }
         }
 
-        @TestInstance(TestInstance.Lifecycle.PER_CLASS)
         @Nested
         @DisplayName("하얀 말 하나, 검은 말 하나를 추가한다면")
         class AddWhiteBlackPawn {
-            Board board = new Board();
 
-            @BeforeAll
+            @BeforeEach
             void addPawns(){
-                Pawn whitePawn = new Pawn(ChessPieceColor.WHITE.getColorName());
-                Pawn blackPawn = new Pawn(ChessPieceColor.BLACK.getColorName());
+                Pawn whitePawn = new Pawn(Color.WHITE.getColorName());
+                Pawn blackPawn = new Pawn(Color.BLACK.getColorName());
                 board.addPawn(whitePawn);
                 board.addPawn(blackPawn);
             }
 
             @Test
-            @DisplayName("보드 위의 폰의 갯수는 1개이다")
+            @DisplayName("보드 위의 폰의 갯수는 2개이다")
             void getBoardSize(){
                 assertThat(board.getNumOfPawn()).isEqualTo(2);
             }
@@ -65,16 +60,41 @@ public class BoardTest {
                 @Test
                 @DisplayName("첫 번째는 하얀색이다")
                 void findFirstPawnColorOnBoard(){
-                    assertThat(board.findPawn(0)).isEqualTo(ChessPieceColor.WHITE.getColorName());
+                    assertThat(board.findPawn(0)).isEqualTo(Color.WHITE.getColorName());
                 }
 
                 @Test
                 @DisplayName("두 번째는 검은색이다")
                 void findSecondPawnColorOnBoard(){
-                    assertThat(board.findPawn(1)).isEqualTo(ChessPieceColor.BLACK.getColorName());
+                    assertThat(board.findPawn(1)).isEqualTo(Color.BLACK.getColorName());
 
                 }
             }
         }
+
+        @Nested
+        @DisplayName("보드를 초기화하면")
+        class initializationTest {
+
+            @BeforeEach
+            void initializeBoard(){
+                board.initialize();
+            }
+
+            @Test
+            @DisplayName("위에서 두번째 줄은 검은색 폰들 8개가 있다")
+            void getBlackPawnResult(){
+                final int BLACK_PAWN_ROW = 1;
+                String candidate = board.print().split("\n")[BLACK_PAWN_ROW];
+                assertThat(candidate).isEqualTo("♟♟♟♟♟♟♟♟");
+            }
+
+            @Test
+            @DisplayName("위에서 일곱번째 줄은 검은색 폰들 8개가 있다")
+            void getWhitePawnResult(){
+                final int WHITE_PAWN_ROW = 6;
+                String candidate = board.print().split("\n")[WHITE_PAWN_ROW];
+                assertThat(candidate).isEqualTo("♙♙♙♙♙♙♙♙");
+            }
     }
 }
