4d896a6688f88af3abc79ae616e1b3e9cfbd36a7
Yeriimii <seojs0511@naver.com>
2024-03-07 00:03:13 +0900

    refactor: Piece 리팩토링
    - 출력용 문자열과 허용된 이름을 갖는 Type 추가
    - 추가된 타입에 따라 의존성이 있는 클래스도 대응 변경
    - 기물의 모양을 책임졌던 `PieceShape` 클래스는 더이상 사용하지 않으므로 제거

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -1,70 +1,72 @@
 package chess.pieces;
 
 import static chess.common.Color.*;
+import static chess.pieces.Piece.Type.*;
 
 import chess.common.Color;
+import java.util.function.Function;
 
 public class Piece {
-    public static final String ALLOWED_PAWN_NAME = "pawn";
-    public static final String ALLOWED_KNIGHT_NAME = "knight";
-    public static final String ALLOWED_BISHOP_NAME = "bishop";
-    public static final String ALLOWED_ROOK_NAME = "rook";
-    public static final String ALLOWED_QUEEN_NAME = "queen";
-    public static final String ALLOWED_KING_NAME = "king";
     private final Color color;
     private final String name;
+    private final Type type;
+    private final Function<Piece, String> representation = piece -> piece.isWhite() ? piece.type.whiteRepresentation : piece.type.blackRepresentation;
 
-    private Piece(Color color, String name) {
+    private Piece(Color color, String name, Type type) {
         this.color = color;
         this.name = name;
+        this.type = type;
     }
 
     public static Piece createWhitePawn() {
-        return new Piece(WHITE, ALLOWED_PAWN_NAME);
+        return new Piece(WHITE, PAWN.allowedName, PAWN);
     }
 
     public static Piece createBlackPawn() {
-        return new Piece(BLACK, ALLOWED_PAWN_NAME);
+        return new Piece(BLACK, PAWN.allowedName, PAWN);
     }
 
     public static Piece createWhiteKnight() {
-        return new Piece(WHITE, ALLOWED_KNIGHT_NAME);
+        return new Piece(WHITE, KNIGHT.allowedName, KNIGHT);
     }
 
     public static Piece createBlackKnight() {
-        return new Piece(BLACK, ALLOWED_KNIGHT_NAME);
+        return new Piece(BLACK, KNIGHT.allowedName, KNIGHT);
     }
 
     public static Piece createWhiteBishop() {
-        return new Piece(WHITE, ALLOWED_BISHOP_NAME);
+        return new Piece(WHITE, BISHOP.allowedName, BISHOP);
     }
 
     public static Piece createBlackBishop() {
-        return new Piece(BLACK, ALLOWED_BISHOP_NAME);
+        return new Piece(BLACK, BISHOP.allowedName, BISHOP);
     }
 
     public static Piece createWhiteRook() {
-        return new Piece(WHITE, ALLOWED_ROOK_NAME);
+        return new Piece(WHITE, ROOK.allowedName, ROOK);
     }
 
     public static Piece createBlackRook() {
-        return new Piece(BLACK, ALLOWED_ROOK_NAME);
+        return new Piece(BLACK, ROOK.allowedName, ROOK);
     }
 
     public static Piece createWhiteQueen() {
-        return new Piece(WHITE, ALLOWED_QUEEN_NAME);
+        return new Piece(WHITE, QUEEN.allowedName, QUEEN);
     }
 
     public static Piece createBlackQueen() {
-        return new Piece(BLACK, ALLOWED_QUEEN_NAME);
+        return new Piece(BLACK, QUEEN.allowedName, QUEEN);
     }
 
     public static Piece createWhiteKing() {
-        return new Piece(WHITE, ALLOWED_KING_NAME);
+        return new Piece(WHITE, KING.allowedName, KING);
     }
 
     public static Piece createBlackKing() {
-        return new Piece(BLACK, ALLOWED_KING_NAME);
+        return new Piece(BLACK, KING.allowedName, KING);
+    }
+    public static Piece createBlank() {
+        return new Piece(NO_COLOR, NO_PIECE.allowedName, NO_PIECE);
     }
 
     public Color getColor() {
@@ -75,8 +77,12 @@ public class Piece {
         return name;
     }
 
+    public Type getType() {
+        return type;
+    }
+
     public String getRepresentation() {
-        return PieceShape.find(this).getUnicode();
+        return representation.apply(this);
     }
 
     public boolean isBlack() {
@@ -85,5 +91,34 @@ public class Piece {
 
     public boolean isWhite() {
         return color.equals(WHITE);
+    }
+
+    public boolean isPawn() {
+        return type.equals(PAWN);
+    }
+
+    public boolean isSameColor(Color color) {
+        return this.color.equals(color);
+    }
+
+    public enum Type {
+        PAWN("♙", "♟", "pawn"),
+        KNIGHT("♘", "♞", "knight"),
+        BISHOP("♗", "♝", "bishop"),
+        ROOK("♖", "♜", "rook"),
+        QUEEN("♕", "♛", "queen"),
+        KING("♔", "♚", "king"),
+        NO_PIECE(".", ".", "blank"),
+        ;
+
+        public final String whiteRepresentation;
+        public final String blackRepresentation;
+        public final String allowedName;
+
+        Type(String whiteRepresentation, String blackRepresentation, String allowedName) {
+            this.whiteRepresentation = whiteRepresentation;
+            this.blackRepresentation = blackRepresentation;
+            this.allowedName = allowedName;
+        }
     }
 }
