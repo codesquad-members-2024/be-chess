533bf95f6a834c15acfb9a024cb93195cc33af17
Yeriimii <seojs0511@naver.com>
2024-03-07 02:12:33 +0900

    refactor: `CreateCommand` 리팩터링
    - 생성하는 스태틱 메서드에서 비교하는 방식에 `Piece` 클래스의 Type 적용

--- src/test/java/chess/pieces/CreateCommandTest.java
+++ src/test/java/chess/pieces/CreateCommandTest.java
@@ -2,37 +2,47 @@ package chess.pieces;
 
 import static chess.common.Color.*;
 import static chess.pieces.CreateCommand.create;
+import static chess.pieces.Piece.Type.*;
 import static org.assertj.core.api.Assertions.*;
 
+import chess.pieces.Piece.Type;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.EnumSource.Mode;
 
 class CreateCommandTest {
 
-    @DisplayName("허용된 기물의 이름으로 기물을 생성할 수 있다")
-    @ParameterizedTest(name = "입력 이름: {0}")
-    @ValueSource(strings = {
-            "pawn", "knight", "bishop",
-            "rook", "queen", "king"
+    @DisplayName("지정한 색상과 타입으로 기물을 생성할 수 있다")
+    @ParameterizedTest(name = "테스트 타입: {0}")
+    @EnumSource(mode = Mode.INCLUDE, names = {
+            "PAWN", "KNIGHT", "BISHOP", "ROOK", "QUEEN", "KING"
     })
-    void create_success(String allowedName) {
+    void create_piece(Type type) {
         // given & when
-        Piece piece = create(BLACK, allowedName);
-        String pieceName = piece.getName();
+        Piece blackPiece = create(BLACK, type);
+        Piece whitePiece = create(WHITE, type);
 
         // then
-        assertThat(pieceName).isEqualTo(allowedName);
+        assertThat(blackPiece).extracting("color").containsExactly(BLACK);
+        assertThat(blackPiece).extracting("name").containsExactly(type.allowedName);
+        assertThat(blackPiece).extracting("type").containsExactly(type);
+
+        assertThat(whitePiece).extracting("color").containsExactly(WHITE);
+        assertThat(whitePiece).extracting("name").containsExactly(type.allowedName);
+        assertThat(whitePiece).extracting("type").containsExactly(type);
     }
 
-    @DisplayName("허용되지 않은 이름으로는 기물을 생성할 수 없다")
-    @ParameterizedTest(name = "입력 이름: {0}")
-    @ValueSource(strings = {
-            "폰", "나이트", "비숍",
-            "룩", "퀸", "킹"
-    })
-    void create_fail(String disallowedName) {
-        assertThatThrownBy(() -> create(BLACK, disallowedName))
-                .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("커맨드로 기물이 아닌 Piece를 생성할 수 있다")
+    @Test
+    void create_no_piece() {
+        // given & when
+        Piece noPiece = create(NO_COLOR, NO_PIECE);
+
+        // then
+        assertThat(noPiece).extracting("color").containsExactly(NO_COLOR);
+        assertThat(noPiece).extracting("name").containsExactly(NO_PIECE.allowedName);
+        assertThat(noPiece).extracting("type").containsExactly(NO_PIECE);
     }
 }
