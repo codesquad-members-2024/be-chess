d3d568def7f51f6c1ced0f78df6d46d60a5f0818
Cat Hanbit <ryudb030@gmail.com>
2024-03-06 17:12:43 +0900

    폰 클래스 생성
    흰색 폰 테스트 통과함

---
+++ src/test/java/PawnTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PawnTest {
+    @Test
+    @DisplayName("흰색 폰이 생성되어야함")
+    void create() {
+        Pawn pawn = new Pawn("White");
+        assertThat(pawn.getColor()).isEqualTo("White");
+    }
+
+}
