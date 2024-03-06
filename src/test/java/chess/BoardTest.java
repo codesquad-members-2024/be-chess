7991f234c0860701dcd348a04dfdf30732725467
mango <esudatcom@gmail.com>
2024-03-06 20:16:43 +0900

    BoardTest 수정 : 흰색 폰과 검은색 폰의 초기화를 테스트 하는 메서드 추가

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -16,19 +16,31 @@ class BoardTest {
         board = new Board();
     }
 
-    private void addAndValidateBoard(Pawn pawn, int expectedSize) {
-        board.add(pawn);
-        assertEquals(expectedSize, board.size());
-        assertEquals(pawn, board.findPawn(expectedSize - 1));
-    }
+//    private void addAndValidateBoard(Pawn pawn, int expectedSize) {
+//        board.add(pawn);
+//        assertEquals(expectedSize, board.size());
+//        assertEquals(pawn, board.findPawn(expectedSize - 1));
+//    }
 
     @Test
     @DisplayName("추가한 폰의 개수와 존재를 검증한다")
     public void create() throws Exception {
-        Pawn white = new Pawn(Pawn.WHITE);
-        addAndValidateBoard(white, 1);
+        Pawn white = new Pawn(Pawn.WHITE, Pawn.WHITE_REPRESENTATION);
+        board.addWhitePawn(white);
+        assertEquals(1, board.size());
+        assertEquals(white, board.findWhitePawn(0));
 
-        Pawn black = new Pawn(Pawn.BLACK);
-        addAndValidateBoard(black, 2);
+        Pawn black = new Pawn(Pawn.BLACK, Pawn.BLACK_REPRESENTATION);
+        board.addBlackPawn(black);
+        assertEquals(2, board.size());
+        assertEquals(black, board.findWhitePawn(1));
+    }
+
+    @Test
+    @DisplayName("흰색 폰은 소문자p로 검은색 폰은 대문자P로 초기화되는지 확인한다")
+    public void initialize() throws Exception {
+        board.initialize();
+        assertEquals("pppppppp", board.getWhitePawnResult());
+        assertEquals("PPPPPPPP", board.getBlackPawnResult());
     }
 }
