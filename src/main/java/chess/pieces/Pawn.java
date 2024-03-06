c1e066c69525c195b985cb600eb37e7566f6dcd0
KDT live <dnjsxo0616@gmail.com>
2024-03-06 19:04:20 +0900

    feat: Pawn 클래스에 representation 추가 - Pawn 색에 따른 출력 문자 부여

--- src/main/java/chess/pieces/Pawn.java
+++ src/main/java/chess/pieces/Pawn.java
@@ -1,19 +1,28 @@
 package chess.pieces;
 
 public class Pawn {
-    public static final Character WHITE_REPRESENTATION = 'p';
-    public static final Character BLACK_REPRESENTATION = 'P';
     private final PawnColor color;
+    private final char representation;
     public Pawn() {
         this.color = PawnColor.WHITE;
+        this.representation = PawnColor.WHITE.getRepresentation();
     }
 
     public Pawn(final PawnColor color) {
         this.color = color;
+        this.representation = color.getRepresentation();
+    }
+
+    public Pawn(PawnColor color, char representation) {
+        this.color = color;
+        this.representation = representation;
     }
 
     public PawnColor getColor() {
         return color;
     }
 
+    public char getRepresentation() {
+        return representation;
+    }
 }
