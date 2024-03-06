76f26eef9b70a60cc5b4953f725d99dc5b507bbd
kimjooyoung <kjy915875@gmail.com>
2024-03-06 14:43:59 +0900

    pr comment 사항 수정

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -9,31 +9,40 @@ import java.util.List;
 public class Board {
 
     private List<Pawn> pawns = new ArrayList<>();
+    private List<Pawn> whitePawns = new ArrayList<>();
+    private List<Pawn> blackPawns = new ArrayList<>();
+    private final int PAWN_CNT = 8;
+    private final int BLACK_PAWN = 1;
+    private final int WHITE_PAWN = 6;
 
-    public void add(Pawn pawn) {
-        pawns.add(pawn);
+    public void add(Pawn pawn, Color color) {
+        if (color.equals(Color.WHITE)) whitePawns.add(pawn);
+        else blackPawns.add(pawn);
     }
 
-    public Pawn findPawn(int idx) {
-        return pawns.get(idx);
+    public Pawn findPawn(int idx, Color color) {
+        if (color.equals(Color.WHITE)) return whitePawns.get(idx - 1);
+        return blackPawns.get(idx - 1);
     }
 
-    public int size() {
-        return pawns.size();
+    public int size(Color color) {
+        if (color.equals(Color.WHITE)) return whitePawns.size();
+        return blackPawns.size();
     }
 
     public void initialize() {
         for (Color color : Color.values()) {
-            for (int i = 0; i < 8; i++) add(new Pawn(color));
+            for (int i = 0; i < PAWN_CNT; i++) add(new Pawn(color), color);
         }
+
     }
 
     public void print() {
         StringBuilder sb = new StringBuilder();
         String dots = "........";
-        for (int i = 0; i < 8; i++) {
-            if (i == 1) sb.append(getBlackPawnsResult());
-            else if (i == 6) sb.append(getWhitePawnsResult());
+        for (int i = 0; i < PAWN_CNT; i++) {
+            if (i == BLACK_PAWN) sb.append(getBlackPawnsResult());
+            else if (i == WHITE_PAWN) sb.append(getWhitePawnsResult());
             else sb.append(dots);
             sb.append("\n");
         }
@@ -43,13 +52,13 @@ public class Board {
 
     public String getBlackPawnsResult() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 8; i++) sb.append(pawns.get(i).getRepresent());
+        for (int i = 0; i < PAWN_CNT; i++) sb.append(blackPawns.get(i).getRepresent());
         return sb.toString();
     }
 
     public String getWhitePawnsResult() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 8; i++) sb.append(pawns.get(i + 8).getRepresent());
+        for (int i = 0; i < PAWN_CNT; i++) sb.append(whitePawns.get(i).getRepresent());
         return sb.toString();
     }
 
--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,40 +1,47 @@
 package chess;
 
-import chess.Board;
 import chess.enums.Color;
 import chess.pieces.Pawn;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.*;
 
 public class BoardTest {
 
     Board board;
 
     @BeforeEach
+    @DisplayName("테스트 전 새로운 보드 생성")
     void initBoard() {
         board = new Board();
     }
 
     @Test
+    @DisplayName("폰이 제대로 생성되는지 검증")
     void create() throws Exception {
-        verifyBoard(new Pawn(Color.WHITE), Color.WHITE, 1);
-        verifyBoard(new Pawn(Color.BLACK), Color.BLACK, 2);
+        addPawn(new Pawn(Color.WHITE), Color.WHITE, 1);
+        addPawn(new Pawn(Color.BLACK), Color.BLACK, 1);
 
     }
 
-    void verifyBoard(Pawn pawn, Color color, int size) {
-        board.add(pawn);
-        assertEquals(size, board.size());
-        assertEquals(pawn, board.findPawn(size - 1));
+    void addPawn(Pawn pawn, Color color, int size) {
+        board.add(pawn, color);
+//        assertEquals(size, board.size());
+//        assertEquals(pawn, board.findPawn(size - 1));
+        assertThat(size).isEqualTo(board.size(color));
+        assertThat(pawn).isEqualTo(board.findPawn(size, color));
     }
 
     @Test
+    @DisplayName("보드가 제대로 초기화되는지 검증")
     public void initialize() throws Exception {
         Board board = new Board();
         board.initialize();
-        assertEquals(String.valueOf(Pawn.WHITE_REPRESENT).repeat(8), board.getWhitePawnsResult());
-        assertEquals(String.valueOf(Pawn.BLACK_REPRESENT).repeat(8), board.getBlackPawnsResult());
+//        assertEquals(String.valueOf(Pawn.WHITE_REPRESENT).repeat(8), board.getWhitePawnsResult());
+//        assertEquals(String.valueOf(Pawn.BLACK_REPRESENT).repeat(8), board.getBlackPawnsResult());
+        assertThat(String.valueOf(Pawn.WHITE_REPRESENT).repeat(8)).isEqualTo(board.getWhitePawnsResult());
+        assertThat(String.valueOf(Pawn.BLACK_REPRESENT).repeat(8)).isEqualTo(board.getBlackPawnsResult());
     }
 }
