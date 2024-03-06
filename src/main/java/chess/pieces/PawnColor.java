35c5c32503f205902d8cdb1ece390302f95afcd8
KDT live <dnjsxo0616@gmail.com>
2024-03-06 19:01:58 +0900

    feat: 화이트, 블랙 폰의 문자 추가 - 화이트는 소문자 p - 블랙은 대문자 P

--- src/main/java/chess/pieces/PawnColor.java
+++ src/main/java/chess/pieces/PawnColor.java
@@ -1,6 +1,16 @@
 package chess.pieces;
 
 public enum PawnColor {
-    WHITE,
-    BLACK;
+    WHITE('p'),
+    BLACK('P');
+
+    private final char representation;
+
+    PawnColor(char representation) {
+        this.representation = representation;
+    }
+
+    public char getRepresentation() {
+        return representation;
+    }
 }
