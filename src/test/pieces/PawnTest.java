580b38aa177f69ec83f8f72530626f53cb4d548f
seondays <agseon9@gmail.com>
2024-03-06 11:19:23 +0900

    test : 폰 생성 테스트에 출력 모양 검증하는 부분 추가
    해당 코드들은 폰을 생성할 때, 색에 따라서 정해진 출력 모양이 올바르게 세팅됨을 확인한다.

--- src/test/pieces/PawnTest.java
+++ src/test/pieces/PawnTest.java
@@ -4,28 +4,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.Assertions.*;
 
 import src.chess.Colors;
+import src.chess.Representation;
 import src.chess.pieces.Pawn;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PawnTest {
 
-    void verifyPawn(final Colors color) {
+    void verifyPawn(final Colors color, final Representation representation) {
         Pawn pawn = new Pawn(color);
+
         assertThat(pawn.getColor()).isEqualTo(color.getColorName());
+        assertThat(pawn.toString()).isEqualTo(representation.getMark());
     }
 
     @Test
-    @DisplayName("생성자로 입력받은 값에 알맞은 색으로 폰이 생성된다.")
+    @DisplayName("생성자로 입력받은 값에 알맞은 색으로 폰이 올바르게 생성된다.")
     void create() {
-        verifyPawn(Colors.WHITE);
-        verifyPawn(Colors.BLACK);
+        verifyPawn(Colors.WHITE, Representation.WHITE_PAWN);
+        verifyPawn(Colors.BLACK, Representation.BLACK_PAWN);
     }
 
     @Test
-    @DisplayName("색이 없는 폰을 생성하려고 하면, 흰색 폰이 생성된다.")
+    @DisplayName("색이 없는 폰을 생성하려고 하면, 흰색 폰이 올바르게 생성된다.")
     void create_기본생성() throws Exception {
         Pawn pawn = new Pawn();
-        assertEquals(pawn.getColor(),"white");
+        assertEquals(pawn.getColor(), Colors.WHITE.getColorName());
+        assertThat(pawn.toString()).isEqualTo(Representation.WHITE_PAWN.getMark());
     }
 }
