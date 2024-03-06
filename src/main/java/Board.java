c0fbc132c988a80af93d30170a6eeb721631dc2f
Cat Hanbit <ryudb030@gmail.com>
2024-03-06 18:44:42 +0900

    보드판 구현
    테스트 통과

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
+    private List<Pawn> pawns = new ArrayList<>();
+
+    public void add(Pawn pawn) {
+        pawns.add(pawn);
+    }
+
+    public int size() {
+        return pawns.size();
+    }
+
+    public Pawn findPawn(int i) {
+        return pawns.get(i);
+    }
 }
