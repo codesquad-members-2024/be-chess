4bbdc3f563daaf7872017243b6622de9e7b62d80
ohjuyoungg <ejrm12@naver.com>
2024-03-06 22:11:28 +0900

    feat: 사용자 입력에 따른 start와 end로 체스 게임을 시작하고 보드를 출력하는 클래스 구현 start, end 문자열을 상수로 설정 사용자의 입력에 따른 보드출력 및 게임 시작 메소드 구현

--- src/main/java/ChessGame.java
+++ src/main/java/ChessGame.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ChessGame {
+import chess.Board;
+
+import java.util.Scanner;
+
+public class ChessGame {
+    private static final String GAME_START = "start";
+    private static final String GAME_END = "end";
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals(GAME_START)) {
+                startGame();
+            } else if (userInput.equals(GAME_END)) {
+                return;
+            } else {
+                System.out.println("start 와 end 둘 중 하나만 입력해주세요.");
+            }
+        }
+    }
+
+    private static void startGame() {
+        Board board = new Board();
+        board.initialize();
+        board.print();
+    }
 }
