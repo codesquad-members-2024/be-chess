180b36026a9202041fe9c7f39e29e3c293f5138f
mango <esudatcom@gmail.com>
2024-03-06 18:28:45 +0900

    PawnTest 클래스 수정 : 색상에 따라 출력값을 검증할 수 있도록 수정

--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -13,13 +13,14 @@ class PawnTest {
     @Test
     @DisplayName("흰색과 검정색 폰이 생성되어야 한다")
     public void create() {
-        verifyPawn(Pawn.WHITE);
-        verifyPawn(Pawn.BLACK);
+        verifyPawn(Pawn.WHITE, Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK, Pawn.BLACK_REPRESENTATION);
     }
 
-    private void verifyPawn(final String color){
-        Pawn pawn = new Pawn(color);
+    private void verifyPawn(final String color, final String representation){
+        Pawn pawn = new Pawn(color, representation);
         AssertionsForClassTypes.assertThat(pawn.getColor()).isEqualTo(color);
+        assertThat(pawn.getRepresentation()).isEqualTo(representation);
     }
 
     @Test
@@ -27,5 +28,6 @@ class PawnTest {
     public void createWhitePawn() throws Exception{
         Pawn pawn = new Pawn();
         Assertions.assertEquals(Pawn.WHITE, pawn.getColor());
+        Assertions.assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation());
     }
 }
