fba4dce4d7e7ff9724efe772c596d5ed130052bd
KDT live <dnjsxo0616@gmail.com>
2024-03-06 17:40:40 +0900

    feat: 체스판 생성 - 2차원 배열 8x8 board 체스판을 생성했습니다. - initialize 메서드를 사용하여 보드를 초기화하고, 흰색 폰과 검은색 폰을 배치합니다.

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,23 +1,51 @@
 package chess;
 
 import chess.pieces.Pawn;
+import chess.pieces.PawnColor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
 
-    private final List<Pawn> board = new ArrayList<>();
+    private final Pawn[][] board = new Pawn[8][8];
+    private final ArrayList<Pawn> whitePawns = new ArrayList<>();
+    private final ArrayList<Pawn> blackPawns = new ArrayList<>();
 
-    public void add(final Pawn pawn) {
-        board.add(pawn);
+
+    public void initialize() {
+        for(int i=0; i<8; i++) {
+            blackPawnAdd(new Pawn(PawnColor.BLACK));
+            whitePawnAdd(new Pawn(PawnColor.WHITE));
+        }
+
+        for(int i=0; i<8; i++) {
+            board[1][i] = blackPawns.get(i);
+            board[6][i] = whitePawns.get(i);
+        }
     }
 
-    public Pawn findPawn(final int findIndex) {
-        return board.get(findIndex);
+    public void whitePawnAdd(final Pawn pawn) {
+        whitePawns.add(pawn);
     }
 
-    public int size() {
-        return board.size();
+    public void blackPawnAdd(final Pawn pawn) {
+        blackPawns.add(pawn);
+    }
+
+    public Pawn findWhitePawn(final int findIndex) {
+        return whitePawns.get(findIndex);
+    }
+
+    public Pawn findBlackPawn(final int findIndex) {
+        return blackPawns.get(findIndex);
+    }
+
+    public int blackPawnSize() {
+        return blackPawns.size();
+    }
+
+    public int whitePawnSize() {
+        return whitePawns.size();
     }
 }
