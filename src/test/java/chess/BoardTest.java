4924a7ad54c7318488e2805be2594ef4e7318014
ehpark <silbroeh@gmail.com>
2024-03-07 02:45:50 +0900

    미사용 테스트코드 주석처리 삭제

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -15,30 +15,6 @@ class BoardTest {
         board = new Board();
     }
 
-    /*
-    @Test
-    @DisplayName("Piece 추가 시 갯수 및 추가한 객체 확인")
-    void create() throws Exception {
-        verifyBoard(Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION, 1);
-        verifyBoard(Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION, 2);
-    }
-
-
-    void verifyBoard(final String color, final String representation, int order) {
-        Piece piece = new Piece(color, representation);
-        board.add(piece);
-        assertEquals(order, board.size());
-        assertEquals(piece, board.findPiece(order-1));
-    }
-
-    @Test
-    @DisplayName("Piece 이외의 다른 객체를 추가하면 컴파일 에러가 발생한다.")
-    void addNotPieceObject() {
-        Integer integer = Integer.valueOf("7");
-//        board.add(integer); // 컴파일 에러 발생
-    }
-     */
-
     @Test
     @DisplayName("체스 판 전체의 초기화 상태 확인")
     public void initialize() throws Exception {
