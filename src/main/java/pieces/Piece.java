14d37da0e4d18fb27a873d10ae5a9297df69f28a
ehpark <silbroeh@gmail.com>
2024-03-06 23:02:46 +0900

    feat : 팩토리 메서드 생성
    생성자 접근제한 변경(private) 및 각 Piece 종류/색상별 팩토리 메서드 추가

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -13,10 +13,58 @@ public class Piece {
     private final String color;
     private final String representation;
 
-    public Piece(Name name, String color, String representation) {
+    private Piece(Name name, String color, String representation) {
         this.name = name;
         this.color = color;
         this.representation = representation;
+    }
+
+    public Piece createWhitePawn() {
+        return new Piece(Name.PAWN, WHITE_COLOR, WHITE_REPRESENTATION);
+    }
+
+    public static Piece createBlackPawn() {
+        return new Piece(Name.PAWN, BLACK_COLOR, BLACK_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(Name.KNIGHT, WHITE_COLOR, WHITE_REPRESENTATION);
+    }
+
+    public static Piece createBlackKnight() {
+        return new Piece(Name.KNIGHT, BLACK_COLOR, BLACK_REPRESENTATION);
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(Name.ROOK, WHITE_COLOR, WHITE_REPRESENTATION);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(Name.ROOK, BLACK_COLOR, BLACK_REPRESENTATION);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(Name.BISHOP, WHITE_COLOR, WHITE_REPRESENTATION);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(Name.BISHOP, BLACK_COLOR, BLACK_REPRESENTATION);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(Name.QUEEN, WHITE_COLOR, WHITE_REPRESENTATION);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(Name.QUEEN, BLACK_COLOR, BLACK_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(Name.KING, WHITE_COLOR, WHITE_REPRESENTATION);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(Name.KING, BLACK_COLOR, BLACK_REPRESENTATION);
     }
 
     public String getColor() {
