03cc95b8991bbe4e32eecb4b77c310ff37a919f3
KDT live <dnjsxo0616@gmail.com>
2024-03-06 17:36:29 +0900

    refactor: Pawn 클래스 상수를 enum 상수로 변경

--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -1,25 +1,17 @@
 package chess.pieces;
 import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PawnTest {
     @Test
-    void create_기본생성자() throws Exception {
-        Pawn pawn = new Pawn();
-        assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
-        assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation());
+    @DisplayName("입력한 색의 폰이 생성되어야 한다")
+    public void create() {
+        verifyPawn(PawnColor.WHITE);
+        verifyPawn(PawnColor.BLACK);
     }
-
-    @Test
-    void create() {
-        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
-        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
+    void verifyPawn(final PawnColor color) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
-
-    void verifyPawn(final String color, final char representation) {
-        Pawn pawn = new Pawn(color, representation);
-        assertEquals(color, pawn.getColor());
-        assertEquals(representation, pawn.getRepresentation());
-    }
-
 }
