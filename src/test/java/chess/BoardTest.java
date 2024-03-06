b338badf7bccff9478613ee837ddb2a0b5e9b42c
yonghyeonpark <pyh4616@naver.com>
2024-03-06 20:41:15 +0900

    test: 추가된 기능에 대한 테스트
    Board 클래스의 initialize, print 메서드의 테스트를 진행합니다.
    initialize: 초기화를 진행했을 때, 체스판의 2, 7번 째 줄에 각각 흰색 폰
    Line, 검은색 폰 Line이 잘 추가되어 있는지 확인합니다.
    print: 체스판을 초기화한 후, 현재 체스판의 상태를 출력합니다.

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,10 +1,13 @@
 package chess;
 
 import chess.pieces.Pawn;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BoardTest {
 
@@ -23,19 +26,35 @@ public class BoardTest {
     @DisplayName("체스판에 저장된 폰의 수를 확인합니다.")
     public void verifyNumberOfPawnsOnBoard() {
         board.addPawn(white);
-        Assertions.assertThat(board.getPawnsSize()).isEqualTo(1);
+        assertThat(board.getPawnsSize()).isEqualTo(1);
 
         board.addPawn(black);
-        Assertions.assertThat(board.getPawnsSize()).isEqualTo(2);
+        assertThat(board.getPawnsSize()).isEqualTo(2);
     }
 
     @Test
     @DisplayName("체스판에 저장된 객체를 확인합니다.")
     public void verifyStoredPawnObjectsOnBoard() {
         board.addPawn(white);
-        Assertions.assertThat(board.findPawn(0)).isEqualTo(white);
+        assertThat(board.findPawn(0)).isEqualTo(white);
 
         board.addPawn(black);
-        Assertions.assertThat(board.findPawn(1)).isEqualTo(black);
+        assertThat(board.findPawn(1)).isEqualTo(black);
+    }
+
+    @Test
+    @DisplayName("체스판을 초기화하면 2, 7번째 줄에 각각 흰색 폰 Line, 검은색 폰 Line이 생성됩니다.")
+    void verifyPawnsOnTheBoardAfterInitializeBoard() {
+        board.initialize();
+        List<List<String>> chessBoard = board.getBoard();
+        assertThat(chessBoard.get(1)).isEqualTo(List.of("p", "p", "p", "p", "p", "p", "p", "p"));
+        assertThat(chessBoard.get(6)).isEqualTo(List.of("P", "P", "P", "P", "P", "P", "P", "P"));
+    }
+
+    @Test
+    @DisplayName("현재 체스판의 상태를 출력합니다.")
+    void printCurrentBoardStatus() {
+        board.initialize();
+        System.out.println(board.print());
     }
 }
