0494fba16ef357747cd97301d4be1b158b8ab435
ehpark <silbroeh@gmail.com>
2024-03-06 23:32:20 +0900

    test : Piece 테스트 수정
    기본생성자 테스트 삭제, 정적 팩토리 메서드를 이용한 테스트로 수정

--- src/test/java/pieces/PieceTest.java
+++ src/test/java/pieces/PieceTest.java
@@ -2,12 +2,11 @@ package pieces;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import pieces.Piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PieceTest {
+    /*
     @Test
     @DisplayName("기본 생성자로 생성할 경우 흰색의 폰이 생성되어야 한다.")
     void create_기본생성자() throws Exception {
@@ -15,17 +14,32 @@ class PieceTest {
         assertEquals(Piece.WHITE_COLOR, piece.getColor());
         assertEquals(Piece.WHITE_REPRESENTATION, piece.getRepresentation());
     }
+     */
 
     @Test
-    @DisplayName("지정된 색상의 폰이 생성되어야 한다.")
+    @DisplayName("지정된 색상의 기물이 생성되어야 한다.")
     void create() {
-        verifyPiece(Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
-        verifyPiece(Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
+        verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
+        verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteKnight(), Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
+        verifyPiece(Piece.createBlackKnight(), Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteRook(), Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
+        verifyPiece(Piece.createBlackRook(), Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteBishop(), Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
+        verifyPiece(Piece.createBlackBishop(), Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteQueen(), Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
+        verifyPiece(Piece.createBlackQueen(), Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteKing(), Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
+        verifyPiece(Piece.createBlackKing(), Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
     }
 
-    void verifyPiece(final String color, final String representation) {
-        Piece piece = new Piece(color, representation);
+    void verifyPiece(Piece piece, final String color, final String representation) {
         assertThat(piece.getColor()).isEqualTo(color);
-        assertEquals(representation, piece.getRepresentation());
+        assertThat(piece.getRepresentation()).isEqualTo(representation);
     }
 }
