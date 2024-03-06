928744a0030874daffd014df31ac608b9dcc7090
Daniel <923daniel@naver.com>
2024-03-06 11:49:01 +0900

    feat: Board 클래스 chessboard에 검은색, 흰색 폰 8개씩 초기화 - initializePawn 매소드는 매개변수로 받은 색의 pawn 위치 초기화. - initialize 매소드는 initializePawn을 이용해 검은색, 흰색 폰 모두   초기화.

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,15 +1,36 @@
 package chess;
 
 import pieces.Pawn;
+import pieces.PawnColor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
     private List<Pawn> pawns;
+    private Pawn[][] chessboard;
 
     Board(){
         pawns = new ArrayList<Pawn>();
+        chessboard = new Pawn[8][8];
+    }
+
+    public void initialize(){
+        initializePawn(PawnColor.WHITE);
+        initializePawn(PawnColor.BLACK);
+    }
+
+    public void initializePawn(PawnColor color){ // 매개변수로 받은 색의 pawn 위치 초기화
+        int initRow = 1; // pawn을 추가할 row, 기본으로 white일때 위치
+        if(color == PawnColor.BLACK){
+            initRow = 6;
+        }
+
+        for(int i=0; i<8; i++){
+            Pawn pawn = new Pawn(color);
+            add(pawn);
+            chessboard[initRow][i] = pawn;
+        }
     }
 
     public void add(Pawn pawn) {
