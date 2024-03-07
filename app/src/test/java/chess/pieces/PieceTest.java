ae9b59a15a95c892f3461aceef7df77a6420bf6f
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-07 18:21:05 +0900

    test : 수정된 형상 정보 주입 메서드 테스트
    - 모든 형상에 대해 테스트 코드 추가

--- app/src/test/java/chess/pieces/PieceTest.java
+++ app/src/test/java/chess/pieces/PieceTest.java
@@ -6,16 +6,25 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PieceTest {
-    @DisplayName("Piece의 팩토리 메서드를 호출하여 흰색, 검은색 Pawn 객체를 생성할 수 있다")
+    @DisplayName("Piece의 팩토리 메서드를 호출하여 이름, 색상에 맞는 기물 객체를 생성할 수 있다")
     @Test
     void create_piece() {
-        verifyPiece(Piece.createWhitePawn(), Color.WHITE);
-        verifyPiece(Piece.createBlackPawn(), Color.BLACK);
+        verifyPiece(Piece.createWhitePawn(), Name.PAWN, Color.WHITE);
+        verifyPiece(Piece.createBlackPawn(), Name.PAWN, Color.BLACK);
+        verifyPiece(Piece.createWhiteKnight(), Name.KNIGHT, Color.WHITE);
+        verifyPiece(Piece.createBlackKnight(), Name.KNIGHT, Color.BLACK);
+        verifyPiece(Piece.createWhiteRook(), Name.ROOK, Color.WHITE);
+        verifyPiece(Piece.createBlackRook(), Name.ROOK, Color.BLACK);
+        verifyPiece(Piece.createWhiteBishop(), Name.BISHOP, Color.WHITE);
+        verifyPiece(Piece.createBlackBishop(), Name.BISHOP, Color.BLACK);
+        verifyPiece(Piece.createWhiteQueen(), Name.QUEEN, Color.WHITE);
+        verifyPiece(Piece.createBlackQueen(), Name.QUEEN, Color.BLACK);
+        verifyPiece(Piece.createWhiteKing(), Name.KING, Color.WHITE);
+        verifyPiece(Piece.createBlackKing(), Name.KING, Color.BLACK);
     }
 
-    private void verifyPiece(final Piece piece, final Color color){
+    private void verifyPiece(final Piece piece, final String name, final Color color){
         assertThat(piece.getColor()).isEqualTo(color);
-        assertThat(piece.getRepresentation()).isEqualTo((Representation.PAWN.inColor(color)));
-        assertThat(piece.getName()).isEqualTo(Name.PAWN);
+        assertThat(piece.getRepresentation()).isEqualTo((Representation.of(name, color)));
     }
 }
