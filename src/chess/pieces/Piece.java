85befdee98cc67f2fa37367f585a8fe2651322ff
seondays <agseon9@gmail.com>
2024-03-07 14:28:14 +0900

    refactor : Type enum 위치 수정
    Piece 내부로 enum의 위치를 수정

--- src/chess/pieces/Piece.java
+++ src/chess/pieces/Piece.java
@@ -3,61 +3,61 @@ package src.chess.pieces;
 public class Piece {
     private Colors color;
     private String representation;
-    private PieceType pieceType;
+    private Type type;
 
-    private Piece(Colors color, PieceType pieceType) {
+    private Piece(Colors color, Type type) {
         this.color = color;
-        this.pieceType = pieceType;
+        this.type = type;
         setRepresentation();
     }
 
     // 생성자를 막고 팩토리 메서드를 추가한다. 색과 이름을 받아서 구현해야 한다.
     public static Piece createWhitePawn() {
-        return new Piece(Colors.WHITE, PieceType.PAWN);
+        return new Piece(Colors.WHITE, Type.PAWN);
     }
 
     public static Piece createBlackPawn() {
-        return new Piece(Colors.BLACK, PieceType.PAWN);
+        return new Piece(Colors.BLACK, Type.PAWN);
     }
 
     public static Piece createBlackKnight() {
-        return new Piece(Colors.BLACK, PieceType.KNIGHT);
+        return new Piece(Colors.BLACK, Type.KNIGHT);
     }
 
     public static Piece createWhiteKnight() {
-        return new Piece(Colors.WHITE, PieceType.KNIGHT);
+        return new Piece(Colors.WHITE, Type.KNIGHT);
     }
 
     public static Piece createBlackRook() {
-        return new Piece(Colors.BLACK, PieceType.ROOK);
+        return new Piece(Colors.BLACK, Type.ROOK);
     }
 
     public static Piece createWhiteRook() {
-        return new Piece(Colors.WHITE, PieceType.ROOK);
+        return new Piece(Colors.WHITE, Type.ROOK);
     }
 
     public static Piece createBlackBishop() {
-        return new Piece(Colors.BLACK, PieceType.BISHOP);
+        return new Piece(Colors.BLACK, Type.BISHOP);
     }
 
     public static Piece createWhiteBishop() {
-        return new Piece(Colors.WHITE, PieceType.BISHOP);
+        return new Piece(Colors.WHITE, Type.BISHOP);
     }
 
     public static Piece createBlackQueen() {
-        return new Piece(Colors.BLACK, PieceType.QUEEN);
+        return new Piece(Colors.BLACK, Type.QUEEN);
     }
 
     public static Piece createWhiteQueen() {
-        return new Piece(Colors.WHITE, PieceType.QUEEN);
+        return new Piece(Colors.WHITE, Type.QUEEN);
     }
 
     public static Piece createBlackKing() {
-        return new Piece(Colors.BLACK, PieceType.KING);
+        return new Piece(Colors.BLACK, Type.KING);
     }
 
     public static Piece createWhiteKing() {
-        return new Piece(Colors.WHITE, PieceType.KING);
+        return new Piece(Colors.WHITE, Type.KING);
     }
 
     public boolean isBlack() {
@@ -71,9 +71,9 @@ public class Piece {
     // 색에 따라서 Representation의 설정이 필요하다
     private void setRepresentation() {
         if (isBlack()) {
-            representation = pieceType.getPieceByColor(Colors.BLACK);
+            representation = type.getPieceByColor(Colors.BLACK);
         } else {
-            representation = pieceType.getPieceByColor(Colors.WHITE);
+            representation = type.getPieceByColor(Colors.WHITE);
         }
     }
 
@@ -84,5 +84,31 @@ public class Piece {
     @Override
     public String toString() {
         return representation;
+    }
+
+    // Enum
+    public enum Type {
+        PAWN("p", "P"),
+        KNIGHT("n", "N"),
+        ROOK("r", "R"),
+        BISHOP("b", "B"),
+        QUEEN("q", "Q"),
+        KING("k", "K");
+
+        private final String whitePiece;
+        private final String blackPiece;
+
+        Type(String whitePiece, String blackPiece) {
+            this.whitePiece = whitePiece;
+            this.blackPiece = blackPiece;
+        }
+
+        // 색을 파라미터로 받아서 해당되는 말의 모양을 return 해주자
+        public String getPieceByColor(Colors color) {
+            if (color.equals(Colors.BLACK)) {
+                return blackPiece;
+            }
+            return whitePiece;
+        }
     }
 }
--- src/test/pieces/PieceTest.java
+++ src/test/pieces/PieceTest.java
@@ -7,14 +7,13 @@ import org.junit.jupiter.api.DisplayName;
 import src.chess.pieces.Colors;
 import src.chess.pieces.Piece;
 import org.junit.jupiter.api.Test;
-import src.chess.pieces.PieceType;
 
 public class PieceTest {
     @Test
     @DisplayName("폰을 생성할 때 올바른 색의 폰인지, 색에 따라 올바른 기물 모양이 세팅되었는지 확인한다.")
     public void create_piece() {
-        verifyPiece(Piece.createWhitePawn(), Colors.WHITE.getColorName(), PieceType.PAWN.getPieceByColor(Colors.WHITE));
-        verifyPiece(Piece.createBlackPawn(), Colors.BLACK.getColorName(), PieceType.PAWN.getPieceByColor(Colors.BLACK));
+        verifyPiece(Piece.createWhitePawn(), Colors.WHITE.getColorName(), Piece.Type.PAWN.getPieceByColor(Colors.WHITE));
+        verifyPiece(Piece.createBlackPawn(), Colors.BLACK.getColorName(), Piece.Type.PAWN.getPieceByColor(Colors.BLACK));
     }
 
     private void verifyPiece(final Piece piece, final String color, final String representation) {
@@ -36,6 +35,13 @@ public class PieceTest {
         Piece knight = Piece.createWhiteKnight();
         assertThat(knight.isWhite()).isTrue();
         assertThat(knight.isBlack()).isFalse();
+    }
+
+    @Test
+    @DisplayName("폰의 기물 모양이 색에 맞게 올바르게 관리되고 있는지 확인한다.")
+    public void getRepresentationPerPiece() throws Exception {
+        assertThat("p").isEqualTo(Piece.Type.PAWN.getPieceByColor(Colors.WHITE));
+        assertThat("P").isEqualTo(Piece.Type.PAWN.getPieceByColor(Colors.BLACK));
     }
 }
 
