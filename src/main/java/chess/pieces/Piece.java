607793f278f84c7ae0252e424a95f71a24b714f5
KDT live <dnjsxo0616@gmail.com>
2024-03-07 13:27:25 +0900

    feat: 팩토리 메서드 생성 - pawn, knight, rook, bishop, queen, king의 흰색폰, 검정폰 생성하는 메서드 생성 - Pawn -> Piece 이름 변경

---
+++ src/main/java/chess/pieces/Piece.java
@@ -0,0 +1,73 @@
+package chess.pieces;
+
+public class Piece {
+    private final PawnColor color;
+    private final PawnType pawnName;
+    private final char representation;
+
+    private Piece(PawnColor color, PawnType pawnName) {
+        this.color = color;
+        this.pawnName = pawnName;
+        this.representation = color.getRepresentation();
+    }
+
+    private static Piece createPawn(PawnColor pawnColor, PawnType pawnName) {
+        return new Piece(pawnColor, pawnName);
+    }
+
+    public static Piece createWhitePawn() {
+        return createPawn(PawnColor.WHITE, PawnType.PAWN);
+    }
+
+    public static Piece createBlackPawn() {
+        return createPawn(PawnColor.BLACK, PawnType.PAWN);
+    }
+
+    public static Piece createWhiteKnight() {
+        return createPawn(PawnColor.WHITE, PawnType.KNIGHT);
+    }
+
+    public static Piece createBlackKnight() {
+        return createPawn(PawnColor.BLACK, PawnType.KNIGHT);
+    }
+
+    public static Piece createWhiteRook() {
+        return createPawn(PawnColor.WHITE, PawnType.ROOK);
+    }
+
+    public static Piece createBlackRook() {
+        return createPawn(PawnColor.BLACK, PawnType.ROOK);
+    }
+
+    public static Piece createWhiteBishop() {
+        return createPawn(PawnColor.WHITE, PawnType.BISHOP);
+    }
+
+    public static Piece createBlackBishop() {
+        return createPawn(PawnColor.BLACK, PawnType.BISHOP);
+    }
+
+    public static Piece createWhiteQueen() {
+        return createPawn(PawnColor.WHITE, PawnType.QUEEN);
+    }
+
+    public static Piece createBlackQueen() {
+        return createPawn(PawnColor.BLACK, PawnType.QUEEN);
+    }
+
+    public static Piece createWhiteKing() {
+        return createPawn(PawnColor.WHITE, PawnType.KING);
+    }
+
+    public static Piece createBlackKing() {
+        return createPawn(PawnColor.BLACK, PawnType.KING);
+    }
+
+    public PawnColor getColor() {
+        return color;
+    }
+
+    public char getRepresentation() {
+        return representation;
+    }
+}
