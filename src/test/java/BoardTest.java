cd279db11c35b44fbc84a3c7d2913bf0357b9e9a
Cat Hanbit <ryudb030@gmail.com>
2024-03-07 11:58:05 +0900

    테스트 코드 중복제거
    BeforeEach를 이용해 보드와 폰 객체를 만들어 중복을 제거함

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,23 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
 import pieces.Pawn;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
+    Board board;
+    Pawn white;
+    Pawn black;
+    @BeforeEach
+    void setUP() {
+        board = new Board();
+        white = new Pawn(Pawn.WHITE_COLOR);
+        black = new Pawn(Pawn.BLACK_COLOR);
+
+    }
     @Test
     public void create() throws Exception {
-        Board board = new Board();
 
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
         board.add(white);
         assertEquals(1, board.size());
         assertEquals(white, board.findPawn(0));
 
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
         board.add(black);
         assertEquals(2, board.size());
         assertEquals(black, board.findPawn(1));
     }
+
 
     @Test
     void Pawn이_아닌_객체_추가_불가() {
