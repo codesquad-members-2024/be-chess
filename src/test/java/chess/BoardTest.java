ac824a5e1efd375b3ada4098ba649134aaa09d55
ohjuyoungg <ejrm12@naver.com>
2024-03-08 00:47:17 +0900

    refactor: public 접근제어자 default로 변경하여 리팩토링

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -3,46 +3,44 @@ package chess;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import pieces.Pawn;
-import pieces.Pawn.*;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static utils.StringUtils.appendNewLine;
 
 public class BoardTest {
-    private chess.Board board;
+    private Board board;
 
     @BeforeEach
     void setUp() {
-        board = new chess.Board();
+        board = new Board();
     }
 
     @Test
     @DisplayName("보드 리스트의 처음 초기값이 0인지 확인한다")
     void initialSize() {
-        board = new chess.Board();
+        board = new Board();
         assertThat(board.size()).isEqualTo(0);
     }
 
-    @Test
-    @DisplayName("보드 생성 후 폰을 색상별로 추가한뒤 크기에 맞는 사이즈와 인덱스에 따른 주소값이 동일한지 검증한다")
-    void create() {
-        Pawn white = new Pawn(Color.WHITE);
-        board.addWhitePawn(white);
-        assertThat(board.sizeWhitePawn()).isEqualTo(1);
-        assertThat(board.findWhitePawn(0)).isEqualTo(white);
-
-        Pawn black = new Pawn(Pawn.Color.BLACK);
-        board.addBlackPawn(black);
-        assertThat(board.sizeBlackPawn()).isEqualTo(1);
-        assertThat(board.findBlackPawn(0)).isEqualTo(black);
-    }
 
     @Test
     @DisplayName("보드 생성 후 폰의 각 색상별 기물이 올바른 값으로 보드에 추가되었는지 확인한다")
     void initialize() {
-        chess.Board board = new chess.Board();
+        Board board = new Board();
         board.initialize();
-        assertThat(board.getWhitePawnsResult()).isEqualTo("pppppppp");
-        assertThat(board.getBlackPawnsResult()).isEqualTo("PPPPPPPP");
+        assertThat(board.size()).isEqualTo(32);
+        String blankRank = appendNewLine("........");
+        assertThat(
+                appendNewLine("RNBQKBNR") +
+                        appendNewLine("PPPPPPPP") +
+                        blankRank + blankRank + blankRank + blankRank +
+                        appendNewLine("pppppppp") +
+                        appendNewLine("rnbqkbnr")).isEqualTo(board.getResultToPrint());
+    }
+
+    @Test
+    void print() {
+        board.initialize();
+        System.out.println(board.getResultToPrint());
     }
 }
