e88a659e912b0b79116cd9faa0996fb26cf42ccb
seondays <agseon9@gmail.com>
2024-03-07 11:52:45 +0900

    refactor : 시작 종료 입력 커멘드를 상수로 설정
    start / end 스트링을 상수로 지정하여 의미를 잘 알 수 있도록 변경

--- src/chess/Application.java
+++ src/chess/Application.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 import src.chess.board.Board;
 
 public class Application {
+    private static final String COMMAND_START = "start";
+    private static final String COMMAND_END = "end";
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Board board = new Board();
@@ -12,19 +15,19 @@ public class Application {
         System.out.println("게임 시작을 원하시면 'start', 종료하시려면 'end'를 입력해 주세요.");
         while (true) {
             String input = Application.validateInput(sc.nextLine());
-            if (input.equals("end")) {
+            if (input.equals(COMMAND_END)) {
                 System.out.println("게임을 종료합니다.");
                 break;
             }
-            if (input.equals("start")) {
+            if (input.equals(COMMAND_START)) {
                 board.initialize();
-                board.print();
+                System.out.println(board.showBoard());
             }
         }
     }
 
     public static String validateInput(String input) {
-        if(!(input.equals("start") || input.equals("end"))) {
+        if(!(input.equals(COMMAND_START) || input.equals(COMMAND_END))) {
             throw new IllegalArgumentException("start 혹은 end만 입력 가능합니다.");
         }
         return input;
