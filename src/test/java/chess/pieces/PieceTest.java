7702517e92846c3896f67b860edf245ee0cf236d
KDT live <dnjsxo0616@gmail.com>
2024-03-07 16:16:27 +0900

    feat: Piece 테스트 추가 - 생성한 폰에 맞는 종류, 색상, 문자를 가진 폰이 생성 테스트를 추가했습니다. - 검은색폰과 흰색폰을 구분하는 메소드 테스트 추가했습니다.

--- src/test/java/chess/pieces/PieceTest.java
+++ src/test/java/chess/pieces/PieceTest.java
@@ -1,19 +1,49 @@
 package chess.pieces;
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PieceTest {
     @Test
-    @DisplayName("흰색폰과 검정폰이 생성되어야한다")
+    @DisplayName("생성한 폰에 맞는 종류와 색상, 문자를 가진 폰이 생성되어야한다")
     void create_piece() {
         verifyPiece(Piece.createWhitePawn(), PawnColor.WHITE, 'p');
         verifyPiece(Piece.createBlackPawn(), PawnColor.BLACK, 'P');
 
+        verifyPiece(Piece.createWhiteKnight(), PawnColor.WHITE, 'n');
+        verifyPiece(Piece.createBlackKnight(), PawnColor.BLACK, 'N');
+
+        verifyPiece(Piece.createWhiteRook(), PawnColor.WHITE, 'r');
+        verifyPiece(Piece.createBlackRook(), PawnColor.BLACK, 'R');
+
+        verifyPiece(Piece.createWhiteBishop(), PawnColor.WHITE, 'b');
+        verifyPiece(Piece.createBlackBishop(), PawnColor.BLACK, 'B');
+
+        verifyPiece(Piece.createWhiteQueen(), PawnColor.WHITE, 'q');
+        verifyPiece(Piece.createBlackQueen(), PawnColor.BLACK, 'Q');
+
+        verifyPiece(Piece.createWhiteKing(), PawnColor.WHITE, 'k');
+        verifyPiece(Piece.createBlackKing(), PawnColor.BLACK, 'K');
     }
 
     private void verifyPiece(final Piece piece, final PawnColor color, final char representation) {
         assertEquals(color, piece.getColor());
         assertEquals(representation, piece.getRepresentation());
+    }
+
+    @Test
+    @DisplayName("흰색폰을 생성한 후 isWhite는 true, isBlack은 false 이어야한다")
+    void isWhiteColor() {
+        Piece whitePawn = Piece.createWhitePawn();
+        assertTrue(whitePawn.isWhite());
+        assertFalse(whitePawn.isBlack());
+    }
+
+    @Test
+    @DisplayName("검정폰을 생성한 후 isWhite는 true, isBlack은 false 이어야한다")
+    void isBlackColor() {
+        Piece blackPawn = Piece.createBlackPawn();
+        assertTrue(blackPawn.isBlack());
+        assertFalse(blackPawn.isWhite());
     }
 }
