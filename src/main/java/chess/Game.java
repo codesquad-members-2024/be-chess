664ea58c1d68a8b363c88baaf398262448f60eeb
kimjooyoung <kjy915875@gmail.com>
2024-03-07 11:33:16 +0900

    move 입력 구현

--- src/main/java/chess/Game.java
+++ src/main/java/chess/Game.java
@@ -1,26 +1,36 @@
 package chess;
 
 import chess.enums.Order;
+import chess.enums.Position;
 
 import java.util.Scanner;
 
 public class Game {
 
+    private String[] moveIndex;
+
     public void run() {
         Order nowOrder = Order.START;
-
-        while (nowOrder.equals(Order.START)) {
+        Board board = new Board();
+        while (!nowOrder.equals(Order.END)) {
             try {
                 nowOrder = userInput();
-                if (nowOrder.equals(Order.START)) {
-                    Board board = new Board();
-                    board.initialize();
-                    System.out.println(board.showBoard());
-                } else {
-                    break;
+                switch (nowOrder) {
+                    case START -> {
+                        board.initialize();
+                        board.printBoard();
+                    }
+                    case END -> {
+                        break;
+                    }
+                    case MOVE -> {
+                        board.move(moveIndex[1], moveIndex[2]);
+                    }
                 }
             } catch (IllegalArgumentException ex) {
                 System.out.println(ex.getMessage());
+            } catch (ArrayIndexOutOfBoundsException index) {
+                System.out.println(index.getMessage());
             }
         }
 
@@ -32,7 +42,11 @@ public class Game {
 
         Order result;
         try {
-            result = Order.valueOf(input.toUpperCase());
+            String[] tmp = input.split(" ");
+            result = Order.valueOf(tmp[0].toUpperCase());
+            if (result.equals(Order.MOVE)) {
+                moveIndex = tmp;
+            }
         } catch (IllegalArgumentException ex) {
             throw new IllegalArgumentException("잘못된 입력입니다");
         }
