8697963e5885e23736457b830f2fde27a4a8e144
Miensoap <miensop51@gmail.com>
2024-03-08 00:16:43 +0900

    gameLauncher 관련 리뷰 반영
    - 프롬프트 추가
    - 시작할 때 도움말 출력
    - start 후 start 불가
    - start 전 move 불가
    - 테스트 코드 작성 : input , output 관련 테스트 학습

--- src/main/java/GameLauncher.java
+++ src/main/java/GameLauncher.java
@@ -9,38 +9,42 @@ public class GameLauncher {
     static ChessGame game;
     static Pattern moveRegex = Pattern.compile("move( [a-h][1-8]){2}");
     public static void main(String[] args) {
+        runChessCmd();
+    }
+    static void runChessCmd() {
         Scanner sc = new Scanner(System.in);
 
+        printHelp();
         while (true){
+            System.out.print("Chess>> ");
             String input = sc.nextLine();
 
-            if(input.equals("start")) runGame();
-            else if(moveRegex.matcher(input).matches() && game!=null) tryMove(input);
-            else if(input.equals("end")) return;
+            if(input.equals("start") && game==null) runGame();
+            else if(moveRegex.matcher(input).matches() && game!=null) game.tryMove(input);
+            else if(input.equals("end")) {
+                System.out.println("GoodBye");
+                return;
+            }
             else System.out.println("잘못된 명령입니다.");
         }
     }
-    /*
-    Chess> 와 같은 프롬프트가 있으면 좋겠어요
-    처음 시작하면 도움말을 표시해 줘도 좋겠어요.
-    상태에 맞지 않는 명령을 못 내리게 하면 좋겠어요. (start- start 라던가, start 없이 move를 한다던가 하는..
-    테스트 코드도 있으면 좋겠어요.
-     */
 
+    private static void printHelp() {
+        System.out.println("""
+                     <체스 게임>
+            ---------------------------
+             게임을 시작하려면 start 입력
+             게임을 종료하려면 end 입력
+                                        
+             게임 시작후 기물을 이동하려면
+             move a1 a2 형식 입력
+            ---------------------------
+            """);
+    }
     private static void runGame() {
         Board board = new Board();
         board.init();
-
         game = new ChessGame(board);
-        game.print();
-    }
-    private static void tryMove(String positions) {
-        StringTokenizer st = new StringTokenizer(positions, " ");
-        st.nextToken();
-        try {
-            game.movePieceAt(st.nextToken(), st.nextToken());
-        }catch (IllegalArgumentException failMove){
-            System.out.println("이동할 수 없는 위치입니다.");
-        }
+        game.printView();
     }
 }
