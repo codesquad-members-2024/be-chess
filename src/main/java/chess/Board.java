599c715f9a9d51e666d4676c5d2e2913dc23d673
sean <tmdgus717@naver.com>
2024-03-07 16:19:53 +0900

    initialize 중복 삭제를 위해 makePawns 메서드 생성
    add메서드를 사용하는 함수를 통해 입력값에 따라 pawns 리스트 생성하게 구현

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -7,7 +7,10 @@ import java.util.stream.Collectors;
 
 public class Board {
     private static final int BOARD_SIZE = 8;
-    private final List<Pawn> pawns = new ArrayList<>(); //final 변경
+    private static final int PAWNS_SIZE = 8;
+    private static final int WHITE_PAWNS_INDEX = 6;
+    private static final int BLACK_PAWNS_INDEX = 1;
+    private final List<Pawn> pawns = new ArrayList<>(PAWNS_SIZE); //final 변경
     private final List<List<Pawn>> board = new ArrayList<>(BOARD_SIZE);
 
     public void add(final Pawn pawn) {
@@ -22,24 +25,26 @@ public class Board {
         return this.pawns.get(index);
     }
 
-    public void initialize() {
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            if(i == 1) {
-                for (int j = 0; j < BOARD_SIZE; j++) {
-                    add(new Pawn(Pawn.BLACK_COLOR,Pawn.BLACK_REPRESENTATION));
-                }
+    public void initialize() { // 중복 삭제
+        for (int column = 0; column < BOARD_SIZE; column++) {
+            if (column == BLACK_PAWNS_INDEX) {
+                makePawns(Pawn.BLACK_COLOR,Pawn.BLACK_REPRESENTATION);
             }
-            if(i == 6) {
-                for (int j = 0; j < BOARD_SIZE; j++) {
-                    add(new Pawn(Pawn.WHITE_COLOR,Pawn.WHITE_REPRESENTATION));
-                }
+            if(column == WHITE_PAWNS_INDEX){
+                makePawns(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
             }
-
             addBoard();
         }
     }
 
+    private void makePawns(String color, char representation) {
+        for (int row = 0; row < PAWNS_SIZE; row++) {
+            add(new Pawn(color, representation));
+        }
+    }
+
     private void addBoard() {
+        //pawns리스트를 copiedPawns에 복사한다 :: clear하면 리스트 내의 모든 값이 사라지므로 복사하여 저장
         List<Pawn> copiedPawns = pawns.stream()
                 .map(pawn -> new Pawn(pawn.getColor(), pawn.getRepresentation()))
                 .collect(Collectors.toList());
@@ -49,11 +54,11 @@ public class Board {
     }
 
     public String getWhitePawnsResult() {
-        return makeRepresentations(board.get(6));
+        return makeRepresentations(board.get(WHITE_PAWNS_INDEX));
     }
 
     public String getBlackPawnsResult() {
-        return makeRepresentations(board.get(1));
+        return makeRepresentations(board.get(BLACK_PAWNS_INDEX));
     }
 
     private String makeRepresentations(List<Pawn> pawns) {
@@ -66,16 +71,16 @@ public class Board {
 
     public String print() {
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < 8; i++) {
-            if(i == 1){
+        for (int column = 0; column < BOARD_SIZE; column++) {
+            if(column == WHITE_PAWNS_INDEX){
                 stringBuilder.append(getBlackPawnsResult()).append("\n");
-                continue;
             }
-            if(i == 6) {
+            if(column == BLACK_PAWNS_INDEX) {
                 stringBuilder.append(getWhitePawnsResult()).append("\n");
-                continue;
             }
-            stringBuilder.append("........").append("\n");
+            if (column != WHITE_PAWNS_INDEX && column != BLACK_PAWNS_INDEX) {
+                stringBuilder.append("........").append("\n");
+            }
         }
         return String.valueOf(stringBuilder);
     }
