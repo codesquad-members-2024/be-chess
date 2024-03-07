54563bb08784689053654bfc723e9ef5f2f512e8
ohjuyoungg <ejrm12@naver.com>
2024-03-07 16:38:04 +0900

    feat: Piece 클래스 팩토리 메소드 구현 각 기물ㅇ에 따라 값 객체 생성 기본 말을 생성하는 생성자 삭제

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -1,29 +1,107 @@
 package pieces;
 
+
 public class Piece {
     private final Color color;
-    public enum Color {
-        WHITE("p"), BLACK("P");
-        private final String representation;
+    private final Type type;
 
-        Color(String representation) {
+    public enum Color {
+        WHITE, BLACK;
+    }
+
+    public enum Type {
+        PAWN('p'),
+        KNIGHT('n'),
+        ROOK('r'),
+        BISHOP('b'),
+        QUEEN('q'),
+        KING('k');
+
+        private final char representation;
+
+        Type(final char representation) {
             this.representation = representation;
         }
 
-        public String getRepresentation() {
+        public char getWhiteRepresentation() {
             return representation;
         }
-    }
 
-    public Piece() {
-        this.color = Color.WHITE;
-    }
-
-    public Piece(Color color) {
-        this.color = color;
+        public char getBlackRepresentation() {
+            return Character.toUpperCase(this.representation);
+        }
     }
 
     public Color getColor() {
         return color;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public char getRepresentation() {
+        return isWhite() ? type.getWhiteRepresentation() : type.getBlackRepresentation();
+    }
+
+    private Piece(Color color, Type type) {
+        this.color = color;
+        this.type = type;
+    }
+
+    private boolean isBlack() {
+        return color == Color.BLACK;
+    }
+
+    private boolean isWhite() {
+        return color == Color.WHITE;
+    }
+
+    public static Piece createWhitePawn() {
+        return new Piece(Color.WHITE, Type.PAWN);
+    }
+
+    public static Piece createBlackPawn() {
+        return new Piece(Color.BLACK, Type.PAWN);
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(Color.WHITE, Type.KNIGHT);
+    }
+
+    public static Piece createBlackKnight() {
+        return new Piece(Color.BLACK, Type.KNIGHT);
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(Color.WHITE, Type.ROOK);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(Color.BLACK, Type.ROOK);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(Color.WHITE, Type.BISHOP);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(Color.BLACK, Type.BISHOP);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(Color.WHITE, Type.QUEEN);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(Color.BLACK, Type.QUEEN);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(Color.WHITE, Type.KING);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(Color.BLACK, Type.KING);
     }
 }
