0bb1e16de95d3591a44469dc4ce247810c6bfb61
yonghyeonpark <pyh4616@naver.com>
2024-03-08 03:51:00 +0900

    refactor: Representation enum의 필드 수정
    다른 동료의 피드백에서, enum의 상수 필드에는 오로지 상수를 나타내는
    내용만을 담고 있어야 한다는 내용을 보고, 해당 내용을 반영하여
    수정했습니다.

--- src/main/java/chess/pieces/Representation.java
+++ src/main/java/chess/pieces/Representation.java
@@ -2,19 +2,13 @@ package chess.pieces;
 
 public enum Representation {
 
-    WHITE_PAWN(Color.WHITE, "p"),
-    BLACK_PAWN(Color.BLACK, "P");
+    WHITE_PAWN("p"),
+    BLACK_PAWN("P");
 
-    private final Color color;
     private final String representation;
 
-    Representation(Color color, String representation) {
-        this.color = color;
+    Representation(String representation) {
         this.representation = representation;
-    }
-
-    public Color getColor() {
-        return color;
     }
 
     public String getRepresentation() {
