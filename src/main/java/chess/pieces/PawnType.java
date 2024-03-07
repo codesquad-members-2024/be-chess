c1ba028d3e17c22997c22102c12df10e895c46f9
KDT live <dnjsxo0616@gmail.com>
2024-03-07 15:37:26 +0900

    feat: 폰의 종류와 표현 문자 enum 추가 - 폰의 종류와 색상에 따른 문자 표현 방법을 위해 enum을 추가했습니다.

--- src/main/java/chess/pieces/PawnType.java
+++ src/main/java/chess/pieces/PawnType.java
@@ -1,10 +1,23 @@
 package chess.pieces;
 
 public enum PawnType {
-    PAWN,
-    KNIGHT,
-    ROOK,
-    BISHOP,
-    QUEEN,
-    KING
+    PAWN('p'),
+    KNIGHT('k'),
+    ROOK('r'),
+    BISHOP('b'),
+    QUEEN('q'),
+    KING('k');
+
+    private final char representation;
+
+    PawnType(char representation) {
+        this.representation = representation;
+    }
+
+    public char getColorRepresentation(PawnColor pawnColor) {
+        if(pawnColor.equals(PawnColor.BLACK)) {
+            return Character.toUpperCase(representation);
+        }
+        return representation;
+    }
 }
