82f90e24f5a14906a0c62af4017c917837565094
KDT live <dnjsxo0616@gmail.com>
2024-03-07 15:41:20 +0900

    refactor: 열거형 상수의 필드 값을 삭제 - PawnType으로 옮겨서 삭제했습니다

--- src/main/java/chess/pieces/PawnColor.java
+++ src/main/java/chess/pieces/PawnColor.java
@@ -1,16 +1,6 @@
 package chess.pieces;
 
 public enum PawnColor {
-    WHITE('p'),
-    BLACK('P');
-
-    private final char representation;
-
-    PawnColor(char representation) {
-        this.representation = representation;
-    }
-
-    public char getRepresentation() {
-        return representation;
-    }
+    WHITE,
+    BLACK;
 }
