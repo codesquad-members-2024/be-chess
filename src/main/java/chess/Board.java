61d056a7ee9e07d92ba42a2a40e3baca91036e34
lotuxsoo <tndus5383@gmail.com>
2024-03-07 14:05:06 +0900

    feature4 commit

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,18 +1,25 @@
 package chess;
 
-import chess.pieces.Pawn;
-import chess.utils.StringUtils;
+import static chess.utils.StringUtils.appendNewLine;
+
+import chess.pieces.Piece;
 import java.util.ArrayList;
 
 public class Board {
     private static final int COL_NUMS = 8;
     private static final String BLANKS = "•".repeat(COL_NUMS);
-    ArrayList<Pawn> whitePawns;
-    ArrayList<Pawn> blackPawns;
+    ArrayList<Piece> whitePieces;
+    ArrayList<Piece> blackPieces;
+    ArrayList<Piece> whitePawns;
+    ArrayList<Piece> blackPawns;
+    private int pieceCount;
 
     public Board() {
-        whitePawns = new ArrayList<Pawn>();
-        blackPawns = new ArrayList<Pawn>();
+        pieceCount = 0;
+        whitePieces = new ArrayList<Piece>();
+        blackPieces = new ArrayList<Piece>();
+        whitePawns = new ArrayList<Piece>();
+        blackPawns = new ArrayList<Piece>();
     }
 
     public void start() {
@@ -25,55 +32,104 @@ public class Board {
     }
 
     void initialize() {
-        for (int i = 0; i < COL_NUMS; i++) {
-            whitePawns.add(new Pawn("white"));
-            blackPawns.add(new Pawn("black"));
-        }
+        for (int i = 0; i < 8; i++) {
+            whitePawns.add(Piece.createWhitePawn());
+            blackPawns.add(Piece.createBlackPawn());
+        } // 16개
+        pieceCount += 16;
+        createWhitePieces(); //8개
+        createBlackPieces(); //8개
+    }
+
+    private void createWhitePieces() {
+        whitePieces.add(Piece.createWhiteRook());
+        whitePieces.add(Piece.createWhiteKnight());
+        whitePieces.add(Piece.createWhiteBishop());
+        whitePieces.add(Piece.createWhiteQueen());
+        whitePieces.add(Piece.createWhiteKing());
+        whitePieces.add(Piece.createWhiteBishop());
+        whitePieces.add(Piece.createWhiteKnight());
+        whitePieces.add(Piece.createWhiteRook());
+        pieceCount += 8;
+    }
+
+    private void createBlackPieces() {
+        blackPieces.add(Piece.createBlackRook());
+        blackPieces.add(Piece.createBlackKnight());
+        blackPieces.add(Piece.createBlackBishop());
+        blackPieces.add(Piece.createBlackQueen());
+        blackPieces.add(Piece.createBlackKing());
+        blackPieces.add(Piece.createBlackBishop());
+        blackPieces.add(Piece.createBlackKnight());
+        blackPieces.add(Piece.createBlackRook());
+        pieceCount += 8;
     }
 
     public void print() {
         // 현재 체스판의 결과를 출력
         StringBuilder sb = new StringBuilder();
-        sb.append(StringUtils.appendNewLine(BLANKS));
-        sb.append(StringUtils.appendNewLine(getBlackPawnsResult()));
+        sb.append(appendNewLine(BLANKS));
+        sb.append(appendNewLine(getPieceResult(blackPawns)));
         for (int i = 0; i < 4; i++) {
-            sb.append(StringUtils.appendNewLine(BLANKS));
+            sb.append(appendNewLine(BLANKS));
         }
-        sb.append(StringUtils.appendNewLine(getWhitePawnsResult()));
-        sb.append(StringUtils.appendNewLine(BLANKS));
+        sb.append(appendNewLine(getPieceResult(whitePawns)));
+        sb.append(appendNewLine(BLANKS));
         System.out.println(sb);
     }
 
-    public String getWhitePawnsResult() {
+    public String getPieceResult(ArrayList<Piece> pieces) {
         StringBuilder sb = new StringBuilder();
-        whitePawns.stream().map(Pawn::getRepresentation).forEach(sb::append);
+        pieces.stream().map(Piece::getRepresentation).forEach(sb::append);
         return sb.toString();
     }
 
-    public String getBlackPawnsResult() {
+    public String showBoard() {
         StringBuilder sb = new StringBuilder();
-        blackPawns.stream().map(Pawn::getRepresentation).forEach(sb::append);
-        return sb.toString();
-    }
-
-
-    public void add(Pawn pawn) {
-        if (pawn.getColor().equals(Pawn.WHITE_COLOR)) {
-            whitePawns.add(pawn);
-        } else if (pawn.getColor().equals(Pawn.BLACK_COLOR)) {
-            blackPawns.add(pawn);
+        sb.append(appendNewLine(getPieceResult(blackPieces)));
+        sb.append(appendNewLine(getPieceResult(blackPawns)));
+        for (int i = 0; i < 4; i++) {
+            sb.append(appendNewLine(BLANKS));
         }
+        sb.append(appendNewLine(getPieceResult(whitePawns)));
+        sb.append(appendNewLine(getPieceResult(whitePieces)));
+        return sb.toString();
     }
 
-    public int size() {
-        return whitePawns.size() + blackPawns.size();
+    public void add(Piece piece) {
+        if (piece.getColor().equals(Piece.WHITE_COLOR)) {
+            whitePawns.add(piece);
+        } else if (piece.getColor().equals(Piece.BLACK_COLOR)) {
+            blackPawns.add(piece);
+        }
+        pieceCount++;
     }
 
-    public Pawn findWhitePawn(int i) {
+    public Piece findWhitePawn(int i) {
         return whitePawns.get(i);
     }
 
-    public Pawn findBlackPawn(int i) {
+    public Piece findBlackPawn(int i) {
         return blackPawns.get(i);
+    }
+
+    public ArrayList<Piece> getWhitePieces() {
+        return whitePieces;
+    }
+
+    public ArrayList<Piece> getBlackPieces() {
+        return blackPieces;
+    }
+
+    public ArrayList<Piece> getWhitePawns() {
+        return whitePawns;
+    }
+
+    public ArrayList<Piece> getBlackPawns() {
+        return blackPawns;
+    }
+
+    public int getPieceCount() {
+        return pieceCount;
     }
 }
--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,8 +1,8 @@
 package chess;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static chess.utils.StringUtils.appendNewLine;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import chess.pieces.Pawn;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,25 +16,23 @@ public class BoardTest {
     }
 
     @Test
-    @DisplayName("흰색,검은색 pawn을 추가하고 보드에서 찾을 수 있다.")
+    @DisplayName("전체 보드의 출력 결과를 확인한다.")
     public void create() throws Exception {
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findWhitePawn(0));
-
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findBlackPawn(0));
+        board.initialize();
+        assertThat(board.getPieceCount()).isEqualTo(32);
+        String blankRank = appendNewLine("••••••••");
+        assertThat(board.showBoard()).isEqualTo(
+                appendNewLine("RNBQKBNR") + appendNewLine("PPPPPPPP") + blankRank + blankRank + blankRank + blankRank
+                        + appendNewLine("pppppppp") + appendNewLine("rnbqkbnr"));
     }
 
     @Test
-    @DisplayName("흰색 pawn열의 결과와 검은색 pawn열의 결과를 검증한다.")
+    @DisplayName("흰색 pawn열의 결과와 검은색 pawn열의 결과를 검증한다")
     public void initialize() throws Exception {
         Board board = new Board();
-        assertEquals("pppppppp", board.getWhitePawnsResult());
-        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
+        board.start();
+        assertThat(board.getPieceResult(board.getWhitePawns())).isEqualTo("pppppppp");
+        assertThat(board.getPieceResult(board.getBlackPawns())).isEqualTo("PPPPPPPP");
     }
 
 }
