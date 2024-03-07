af9927a8bc75c4aa52ec82d976fb579d82f5c765
mango <esudatcom@gmail.com>
2024-03-07 15:03:53 +0900

    Piece 클래스 수정 : 기물별 색과 이름을 받아 Piece 객체 생성하는 팩토리 메소드 구현

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -35,5 +35,54 @@ public class Piece {
     public String getRepresentation() {
         return representation;
     }
+
+    public static Piece createWhitePawn(){
+        return new Piece(WHITE, WHITE_PAWN_REPRESENTATION);
+    }
+
+    public static Piece createBlackPawn(){
+        return new Piece(BLACK, BLACK_PAWN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKnight(){
+        return new Piece(WHITE, WHITE_KNIGHT_REPRESENTATION);
+    }
+
+    public static Piece createBlackKnight(){
+        return new Piece(BLACK, BLACK_KNIGHT_REPRESENTATION);
+    }
+
+    public static Piece createWhiteRook(){
+        return new Piece(WHITE, WHITE_ROOK_REPRESENTATION);
+    }
+
+    public static Piece createBlackRook(){
+        return new Piece(BLACK, BLACK_ROOK_REPRESENTATION);
+    }
+
+    public static Piece createWhiteBishop(){
+        return new Piece(WHITE, WHITE_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createBlackBishop(){
+        return new Piece(BLACK, BLACK_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createWhiteQueen(){
+        return new Piece(WHITE, WHITE_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createBlackQueen(){
+        return new Piece(BLACK, BLACK_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKing(){
+        return new Piece(WHITE, WHITE_KING_REPRESENTATION);
+    }
+
+    public static Piece createBlackKing(){
+        return new Piece(BLACK, BLACK_KING_REPRESENTATION);
+    }
+
 }
 
