70394a32bf73a13c234564da27d2231e8c888f99
Yeriimii <seojs0511@naver.com>
2024-03-08 14:48:04 +0900

    feat: main()의 move 입력 기능 추가
    - `move 좌표1 좌표2`를 입력하면 기물을 이동시키고, 그 결과를 출력

--- src/main/java/chess/ChessSimulator.java
+++ src/main/java/chess/ChessSimulator.java
@@ -1,6 +1,8 @@
 package chess;
 
 import chess.board.Board;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ChessSimulator {
@@ -18,14 +20,26 @@ public class ChessSimulator {
             if (command.equals("start")) {
                 BOARD.print();
             }
+            if (command.startsWith("move")) {
+                List<String> posList = getPosList(command);
+                BOARD.move(posList.get(0), posList.get(1));
+                BOARD.print();
+            }
             if (command.equals("end")) {
                 break;
             }
         }
     }
 
+    private static List<String> getPosList(String command) {
+        return Arrays.stream(command.split(" "))
+                .skip(1)
+                .toList();
+    }
+
     public static String inputCommand() {
-        System.out.println("명령어를 입력해주세요. [start] / [end]");
+        System.out.println("명령어를 입력해주세요.");
+        System.out.println("[start: 출력]\n[move 좌표1 좌표2: 좌표1 기물을 좌표2로 이동]\n[end: 종료]");
         return INPUT.nextLine();
     }
 }
