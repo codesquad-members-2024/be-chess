d7ad3b0974f21d2104caa2101754cf878f2e2d31
seondays <agseon9@gmail.com>
2024-03-08 12:06:06 +0900

    test : 원하는 위치에 기물을 올바르게 올릴 수 있는지 테스트하는 코드 작성
    해당 테스트는 체스판의 원하는 위치에 기물을 올리는 기능이 정상적인지 확인한다.

--- src/test/board/BoardTest.java
+++ src/test/board/BoardTest.java
@@ -18,12 +18,12 @@ public class BoardTest {
     @BeforeEach
     public void create() {
         board = new Board();
-        board.initialize();
     }
 
     @Test
     @DisplayName("체스판에 정상적으로 모든 기물이 올라간 상태인지 확인한다.")
     public void createBoard() throws Exception {
+        board.initialize();
         assertThat(32).isEqualTo(board.pieceCount());
         String blankRank = StringUtils.appendNewLine("........");
         assertThat(
@@ -37,6 +37,7 @@ public class BoardTest {
     @Test
     @DisplayName("검은색 나이트가 체스판에 몇 개가 있는지 확인한다.")
     public void checkBlackKnightPieceCount() {
+        board.initialize();
         assertThat(board.getPieceCount(Colors.BLACK, Type.KNIGHT))
                 .isEqualTo(2);
     }
@@ -44,6 +45,7 @@ public class BoardTest {
     @Test
     @DisplayName("흰색 폰이 체스판에 몇 개가 있는지 확인한다.")
     public void checkPieceCount() {
+        board.initialize();
         assertThat(board.getPieceCount(Colors.WHITE, Type.PAWN))
                 .isEqualTo(8);
     }
@@ -51,9 +53,24 @@ public class BoardTest {
     @Test
     @DisplayName("특정 위치를 제공하면 체스판의 해당 위치에 어떤 기물이 있는지 확인한다.")
     public void checkBlackRookLocation() {
+        board.initialize();
         assertThat(Piece.createBlack(Type.ROOK)).isEqualTo(board.findPieceBy("a8"));
         assertThat(Piece.createBlack(Type.ROOK)).isEqualTo(board.findPieceBy("h8"));
         assertThat(Piece.createWhite(Type.ROOK)).isEqualTo(board.findPieceBy("a1"));
         assertThat(Piece.createWhite(Type.ROOK)).isEqualTo(board.findPieceBy("h1"));
+    }
+
+    @Test
+    @DisplayName("빈 체스판을 만들어서 그 위에 원하는 위치에 기물을 올바르게 올릴 수 있다.")
+    public void move() throws Exception {
+        board.initializeEmpty();
+        System.out.println(board.showBoard());
+
+        String position = "b5";
+        Piece piece = Piece.createBlack(Type.ROOK);
+        board.move(position, piece);
+
+        assertThat(piece).isEqualTo(board.findPieceBy(position));
+        System.out.println(board.showBoard());
     }
 }
