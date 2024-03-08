1f823289d0094ab440bc777db785c1a31ce51ac8
Daniel <923daniel@naver.com>
2024-03-08 13:11:56 +0900

    refactor: PawnTest 클래스 삭제 후 Piece 클래스 생성

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -1,35 +1,34 @@
-//package pieces;
-//
-//import org.junit.jupiter.api.*;
-//
-//import static org.assertj.core.api.Assertions.*;
-//
-//public class PawnTest {
-//
-//    @Test
-//    @DisplayName("흰색 또는 검은색 폰이 생성되어야 한다")
-//    public void create() {
-//        verifyPawn(PieceColor.WHITE);
-//        verifyPawn(PieceColor.BLACK);
-//    }
-//
-//    @Test
-//    @DisplayName("색이 없는 Pawn을 생성하는 경우 white Pawn을 생성해야 한다 ")
-//    public void create_기본생성자() throws Exception {
-//        Piece piece = new Piece();
-//        assertThat(piece.getColor()).isEqualTo(PieceColor.WHITE);
-//        assertThat(piece.getChessPiece()).isEqualTo(Piece.WHITE_PAWN);
-//    }
-//
-//    private void verifyPawn(PieceColor color) {
-//        Piece piece = new Piece(color);
-//
-//        assertThat(piece.getColor()).isEqualTo(color); // 색 비교
-//
-//        if(color == PieceColor.WHITE){ // chessPiece 비교
-//            assertThat(piece.getChessPiece()).isEqualTo(Piece.WHITE_PAWN);
-//        }else{
-//            assertThat(piece.getChessPiece()).isEqualTo(Piece.BLACK_PAWN);
-//        }
-//    }
-//}
+package pieces;
+
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class PawnTest {
+
+    @Test
+    @DisplayName("흰색 또는 검은색 폰이 생성되어야 한다")
+    public void create() {
+        verifyPawn(PieceColor.WHITE);
+        verifyPawn(PieceColor.BLACK);
+    }
+
+    @Test
+    @DisplayName("색이 없는 Pawn을 생성하는 경우 white Pawn을 생성해야 한다 ")
+    public void create_기본생성자() throws Exception {
+        Piece piece = new Piece();
+        assertThat(piece.getColor()).isEqualTo(PieceColor.WHITE);
+        assertThat(piece.getChessPiece()).isEqualTo(Piece.WHITE_PAWN);
+    }
+
+    private void verifyPawn(PieceColor color) {
+        Piece piece = new Piece(color);
+
+        assertThat(piece.getColor()).isEqualTo(color); // 색 비교
+
+        if(color == PieceColor.WHITE){ // chessPiece 비교
+            assertThat(piece.getChessPiece()).isEqualTo(Piece.WHITE_PAWN);
+        }else{
+            assertThat(piece.getChessPiece()).isEqualTo(Piece.BLACK_PAWN);
+        }
+    }
+}
