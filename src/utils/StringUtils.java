ad0087776ba47d7e2358416bcba0e13a19871f12
seondays <agseon9@gmail.com>
2024-03-08 11:23:11 +0900

    feat : 좌표를 받아 조회를 위한 숫자 인덱스를 반환해주기 위한 유틸리티 기능 구현

--- src/utils/StringUtils.java
+++ src/utils/StringUtils.java
@@ -8,4 +8,14 @@ public class StringUtils {
     public static String appendNewLine(String string) {
         return string + NEWLINE;
     }
+
+    // 영어를 받아서(a ~ h) 숫자로 변환해 준다. 예를 들면 a는 0이 되어야 한다.
+    public static int parserToColIndex(char input){
+        return input - 97;
+    }
+
+    // 숫자를 받아서 인덱스로 변환해 준다. 예를 들면 1은 0이 되어야 한다.
+    public static int parserToRowIndex(char input) {
+        return Character.getNumericValue(input) - 1;
+    }
 }
