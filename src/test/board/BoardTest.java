dd480dd426986af05818bc4d5c88679087ad1f97
seondays <agseon9@gmail.com>
2024-03-08 11:24:08 +0900

    test : 특정 기물의 개수를 확인하는 테스트 코드, 특정 위치의 기물의 타입을 확인하는 테스트 코드 작성

--- src/test/board/BoardTest.java
+++ src/test/board/BoardTest.java
@@ -1,12 +1,14 @@
 package src.test.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import src.chess.board.Board;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import src.chess.pieces.Piece;
+import src.chess.pieces.Piece.Colors;
+import src.chess.pieces.Piece.Type;
 import src.utils.StringUtils;
 
 public class BoardTest {
@@ -16,13 +18,13 @@ public class BoardTest {
     @BeforeEach
     public void create() {
         board = new Board();
+        board.initialize();
     }
 
     @Test
     @DisplayName("체스판에 정상적으로 모든 기물이 올라간 상태인지 확인한다.")
     public void createBoard() throws Exception {
-        board.initialize();
-        assertEquals(32, board.pieceCount());
+        assertThat(32).isEqualTo(board.pieceCount());
         String blankRank = StringUtils.appendNewLine("........");
         assertThat(
                 StringUtils.appendNewLine("RNBQKBNR") +
@@ -30,5 +32,35 @@ public class BoardTest {
                         blankRank + blankRank + blankRank + blankRank +
                         StringUtils.appendNewLine("pppppppp") +
                         StringUtils.appendNewLine("rnbqkbnr")).isEqualTo(board.showBoard());
+    }
+
+    @Test
+    @DisplayName("검은색 나이트가 체스판에 몇 개가 있는지 확인한다.")
+    public void checkBlackKnightPieceCount() {
+        assertThat(board.getPieceCount(Colors.BLACK, Type.KNIGHT))
+                .isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("흰색 폰이 체스판에 몇 개가 있는지 확인한다.")
+    public void checkPieceCount() {
+        assertThat(board.getPieceCount(Colors.WHITE, Type.PAWN))
+                .isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("e1 위치에 하얀 킹이 있는지 확인한다.")
+    public void checkWhiteKingLocation() {
+        Piece piece = board.findPieceBy("e1");
+        assertThat(piece.getType()).isEqualTo(Type.KING);
+        assertThat(piece.getColor()).isEqualTo(Colors.WHITE.getColorName());
+    }
+
+    @Test
+    @DisplayName("a8 위치에 검은 룩이 있는지 확인한다.")
+    public void checkBlackRookLocation() {
+        Piece piece = board.findPieceBy("a8");
+        assertThat(piece.getType()).isEqualTo(Type.ROOK);
+        assertThat(piece.getColor()).isEqualTo(Colors.BLACK.getColorName());
     }
 }
