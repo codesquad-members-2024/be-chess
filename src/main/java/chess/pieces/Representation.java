a9db1711a98b96c5407fb1d545ffff477dab978b
yonghyeonpark <pyh4616@naver.com>
2024-03-08 13:58:58 +0900

    refactor: 기물 형태 추가
    Pawn에 더하여 Knight, Rook, Bishop, Queen, King의 형태도 모두
    추가해줬습니다.

--- src/main/java/chess/pieces/Representation.java
+++ src/main/java/chess/pieces/Representation.java
@@ -3,7 +3,17 @@ package chess.pieces;
 public enum Representation {
 
     WHITE_PAWN("p"),
-    BLACK_PAWN("P");
+    BLACK_PAWN("P"),
+    WHITE_KNIGHT("k"),
+    BLACK_KNIGHT("K"),
+    WHITE_ROOK("r"),
+    BLACK_ROOK("R"),
+    WHITE_BISHOP("b"),
+    BLACK_BISHOP("B"),
+    WHITE_QUEEN("q"),
+    BLACK_QUEEN("Q"),
+    WHITE_KING("k"),
+    BLACK_KING("K");
 
     private final String representation;
 
