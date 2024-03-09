d2ba3ac0287c007ade1abbf555465a7dff4fc982
yonghyeonpark <pyh4616@naver.com>
2024-03-09 17:12:24 +0900

    feat: 체스판에 다른 기물 추가
    나머지 다른 기물들도 체스판에 추가하여 체스 게임의 초기 상태를
    설정합니다.

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,70 +1,102 @@
 package chess;
 
-import chess.pieces.Representation;
+import chess.pieces.Piece;
 import utils.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Board {
 
-    private static final String BLANK = "-";
     private static final int START_INDEX = 0;
     private static final int END_INDEX = 7;
     private static final int INITIAL_BLACK_PAWNS_INDEX = 1;
     private static final int INITIAL_WHITE_PAWNS_INDEX = 6;
 
-    private final List<List<String>> board;
+    private final List<List<Piece>> board;
 
     public Board() {
         this.board = new ArrayList<>();
     }
 
     public void initialize() {
-        List<String> basicLine = getBasicLine();
+        List<Piece> basicLine = getBasicLine();
         for (int i = START_INDEX; i <= END_INDEX; i++) {
             board.add(basicLine);
         }
         board.set(INITIAL_BLACK_PAWNS_INDEX, getBlackPawns());
         board.set(INITIAL_WHITE_PAWNS_INDEX, getWhitePawns());
+        board.set(START_INDEX, getFirstLine());
+        board.set(END_INDEX, getLastLine());
     }
 
-    private List<String> getBasicLine() {
-        List<String> basic = new ArrayList<>();
+    private List<Piece> getBasicLine() {
+        List<Piece> basic = new ArrayList<>();
         for (int i = START_INDEX; i <= END_INDEX; i++) {
-            basic.add(BLANK);
+            basic.add(Piece.createBlank());
         }
         return basic;
     }
 
-    private List<String> getWhitePawns() {
-        List<String> whitePawns = new ArrayList<>();
+    private List<Piece> getWhitePawns() {
+        List<Piece> whitePawns = new ArrayList<>();
         for (int i = START_INDEX; i <= END_INDEX; i++) {
-            whitePawns.add(Representation.WHITE_PAWN.getRepresentation());
+            whitePawns.add(Piece.createWhitePawn());
         }
         return whitePawns;
     }
 
-    private List<String> getBlackPawns() {
-        List<String> blackPawns = new ArrayList<>();
+    private List<Piece> getBlackPawns() {
+        List<Piece> blackPawns = new ArrayList<>();
         for (int i = START_INDEX; i <= END_INDEX; i++) {
-            blackPawns.add(Representation.BLACK_PAWN.getRepresentation());
+            blackPawns.add(Piece.createBlackPawn());
         }
         return blackPawns;
     }
 
+    private List<Piece> getFirstLine() {
+        List<Piece> firstLine = new ArrayList<>();
+        firstLine.add(0, Piece.createBlackRook());
+        firstLine.add(1, Piece.createBlackKnight());
+        firstLine.add(2, Piece.createBlackBishop());
+        firstLine.add(3, Piece.createBlackQueen());
+        firstLine.add(4, Piece.createBlackKing());
+        firstLine.add(5, Piece.createBlackBishop());
+        firstLine.add(6, Piece.createBlackKnight());
+        firstLine.add(7, Piece.createBlackRook());
+        return firstLine;
+    }
+
+    private List<Piece> getLastLine() {
+        List<Piece> lastLine = new ArrayList<>();
+        lastLine.add(0, Piece.createWhiteRook());
+        lastLine.add(1, Piece.createWhiteKnight());
+        lastLine.add(2, Piece.createWhiteBishop());
+        lastLine.add(3, Piece.createWhiteQueen());
+        lastLine.add(4, Piece.createWhiteKing());
+        lastLine.add(5, Piece.createWhiteBishop());
+        lastLine.add(6, Piece.createWhiteKnight());
+        lastLine.add(7, Piece.createWhiteRook());
+        return lastLine;
+    }
+
     public String print() {
         StringBuilder result = new StringBuilder();
-        for (List<String> line : board) {
-            for (String element : line) {
-                result.append(element);
+        for (List<Piece> line : board) {
+            for (Piece piece : line) {
+                result.append(piece.getRepresentation().getRepresentation());
             }
             result.append(StringUtils.NEWLINE);
         }
         return result.toString();
     }
 
-    public boolean verifyBoardLineContent(int line, List<String> content) {
-        return board.get(line - 1).equals(content);
+    public boolean verifyBoardLineContent(int line, String content) {
+        return board.get(line - 1).stream()
+                .map(piece -> piece.getRepresentation().getRepresentation())
+                .collect(Collectors.joining(", "))
+                .equals(content);
+
     }
 }
