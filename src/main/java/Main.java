d588612084684ae468f2b901916ee815e8dad02b
yonghyeonpark <pyh4616@naver.com>
2024-03-09 17:40:08 +0900

    refactor: Main 클래스 내용 추가
    Game과 Board 클래스를 사용하여 초기 세팅을 합니다.
    진행 과정을 구상했습니다. (아직 미구현입니다.)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,29 @@
+import chess.Board;
+import chess.Game;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Board board = new Board();
+        Game game = new Game(board);
+        game.start();
 
         while (true) {
-            String input = scanner.nextLine();
+
+            // 이동 명령어
+            String command = scanner.nextLine();
+
+            game.printMovingResult();
+
+            // 턴을 넘김(구현 예정)
+
+            // 종료 조건 확인
+            if (game.isEnd()) {
+                break;
+            }
         }
     }
 }
