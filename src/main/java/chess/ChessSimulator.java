eb7b23f0ef9ae5fbe2987a742fba32073fedabf6
Yeriimii <seojs0511@naver.com>
2024-03-10 23:47:53 +0900

    refactor: Board, Game 분리
    - Game은 체스판의 점수를 관리한다

--- src/main/java/chess/ChessSimulator.java
+++ src/main/java/chess/ChessSimulator.java
@@ -1,29 +1,33 @@
 package chess;
 
 import chess.board.Board;
+import chess.board.Game;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class ChessSimulator {
     private static final Scanner INPUT = new Scanner(System.in);
-    private static final Board BOARD = new Board();
+    private final Board board = new Board();
+    private final Game game = new Game(board.getBoardBlocks());
 
     public static void main(String[] args) {
-        run();
+        ChessSimulator chessSimulator = new ChessSimulator();
+
+        chessSimulator.run();
     }
 
-    public static void run() {
-        BOARD.initialize();
+    public void run() {
+        board.initialize();
         while (true) {
             String command = inputCommand();
             if (command.equals("start")) {
-                BOARD.print();
+                board.print();
             }
             if (command.startsWith("move")) {
                 List<String> posList = getPosList(command);
-                BOARD.move(posList.get(0), posList.get(1));
-                BOARD.print();
+                game.move(posList.get(0), posList.get(1));
+                board.print();
             }
             if (command.equals("end")) {
                 break;
@@ -31,13 +35,13 @@ public class ChessSimulator {
         }
     }
 
-    private static List<String> getPosList(String command) {
+    private List<String> getPosList(String command) {
         return Arrays.stream(command.split(" "))
                 .skip(1)
                 .toList();
     }
 
-    public static String inputCommand() {
+    public String inputCommand() {
         System.out.println("명령어를 입력해주세요.");
         System.out.println("[start: 출력]\n[move 좌표1 좌표2: 좌표1 기물을 좌표2로 이동]\n[end: 종료]");
         return INPUT.nextLine();
---
+++ src/test/java/chess/board/GameTest.java
@@ -0,0 +1,92 @@
+package chess.board;
+
+import static chess.common.Color.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import chess.pieces.PieceFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    private Board board;
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+        game = new Game(board.getBoardBlocks());
+    }
+
+
+    @DisplayName("검정색 기물에 대해 폰이 아닌 킹, 퀸, 나이트, 비숍, 룩의 합은 19.5점이다")
+    @Test
+    void calculateMajorPoints() {
+        // given
+        board.initializeBoardBlocks();
+        board.setPiece("a1", PieceFactory.createBlackKing());
+        board.setPiece("a2", PieceFactory.createBlackQueen());
+        board.setPiece("a3", PieceFactory.createBlackKnight());
+        board.setPiece("a4", PieceFactory.createBlackBishop());
+        board.setPiece("a5", PieceFactory.createBlackRook());
+
+        // when
+        double majorPoints = game.calculateMajorPoints(BLACK);
+
+        // then
+        assertEquals(19.5, majorPoints, 0.01);
+    }
+
+    @DisplayName("검정색 기물에 대해, 폰이 같은 열에 3개 있으면 합은 1.5점이다")
+    @Test
+    void calculatePawnPoints_when_same_file() {
+        // given
+        board.initializeBoardBlocks();
+        board.setPiece("a1", PieceFactory.createBlackPawn());
+        board.setPiece("a2", PieceFactory.createBlackPawn());
+        board.setPiece("a3", PieceFactory.createBlackPawn());
+
+        // when
+        double pawnPoints = game.calculatePawnPoints(BLACK);
+
+        // then
+        assertEquals(1.5, pawnPoints, 0.01);
+    }
+
+    @DisplayName("검은색 기물에 대해, 폰이 같은 행에 3개 있으면 합은 3.0점이다")
+    @Test
+    void calculatePawnPoints_when_different_file() {
+        // given
+        board.initializeBoardBlocks();
+        board.setPiece("a1", PieceFactory.createBlackPawn());
+        board.setPiece("b1", PieceFactory.createBlackPawn());
+        board.setPiece("c1", PieceFactory.createBlackPawn());
+
+        // when
+        double pawnPoints = game.calculatePawnPoints(BLACK);
+
+        // then
+        assertEquals(3.0, pawnPoints, 0.01);
+    }
+
+    @DisplayName("검정색 기물에 대해, 나이트 2개, 룩 1개, 퀸 1개, 같은 열의 폰 2개의 점수 합은 20.0 이다")
+    @Test
+    void calculatePoints() {
+        // given
+        board.initializeBoardBlocks();
+        board.setPiece("a1", PieceFactory.createBlackKnight());
+        board.setPiece("a2", PieceFactory.createBlackKnight());
+        board.setPiece("a3", PieceFactory.createBlackRook());
+        board.setPiece("a4", PieceFactory.createBlackQueen());
+
+        board.setPiece("b1", PieceFactory.createBlackPawn());
+        board.setPiece("b2", PieceFactory.createBlackPawn());
+
+        // when
+        double points = game.calculatePoints(BLACK);
+
+        // then
+        assertEquals(20.0, points, 0.01);
+    }
+}
